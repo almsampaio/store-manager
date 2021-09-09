8e000ffaf35ea197ac21931872af8cd5dcc7f861
maricegalla <maricegalla@gmail.com>
2021-09-09 17:08:21 -0300

    Index basic structure

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log(`Mãe tá on na porta ${PORT}!`);
 });
