d0689448188a661b5a19d130f4598fc98af7eabd
maricegalla <maricegalla@gmail.com>
2021-09-09 17:13:17 -0300

    Products Service Create

---
+++ services/productsService.js
@@ -0,0 +1,42 @@
+const productsModel = require('../models/productsModel');
+
+const create = async (name, quantity) => {
+  const minimumNameLength = 5;
+  const minimumQuantity = 1;
+
+  if (name.length < minimumNameLength) {
+    return {
+      err:{
+        code: 'invalid_data',
+        message: '"name" length must be at least 5 characters long'
+      }};
+  }
+
+  if (quantity < minimumQuantity) {
+    return {
+      err:{
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1'
+      }};
+  }
+
+  if (typeof(quantity) === 'string') {
+    return {
+      err:{
+        code: 'invalid_data',
+        message: '"quantity" must be a number'
+      }};
+  }
+
+  const exists = await productsModel.findByName(name);
+  if (exists) {
+    return {
+      err:{
+        code: 'invalid_data',
+        message: 'Product already exists'
+      }};
+  }
+
+  const product = await productsModel.create(name,quantity);
+  return { product };
+};
