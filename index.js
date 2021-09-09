4c6d4703d2d50fc7b8640c312a25ffeefa309046
guilhermemd <guilherme.md@gmail.com>
2021-09-09 19:05:01 -0300

    feat: :sparkles: productsRouter Implementation

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+const PORT = 3001;
+const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const productsRoute = require('./routes/productsRoute');
+
+app.use('/products', productsRoute);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-// commit
+
+app.listen(PORT, () => {
+  console.log(`Online na porta ${PORT}`);
+  console.log(`Acessar: http://localhost:${PORT}`);
+});
