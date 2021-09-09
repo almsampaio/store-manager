7151d51a64f227e1152b23c269589e3cb0ede4f8
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-09 16:46:34 -0300

    feat: middlewares validate

---
+++ middlewares/validate.js
@@ -0,0 +1,41 @@
+const HTTP_VALID_STATUS = 422;
+
+const MIN_NAME_LENGTH = 5;
+const MIN_QUANT_ZERO = 0;
+
+const name = (req, res, next) => {
+  const { name } = req.body;
+  if (typeof name !== 'string') {
+    return res.status(HTTP_VALID_STATUS).json({
+      mensage: '"name" must be a string'
+    });
+  };
+  if (name.length <= MIN_NAME_LENGTH) {
+    return res.status(HTTP_VALID_STATUS).json({
+      mensage: '"name" length must be at least 5 characters long'
+    });
+  };
+
+  next();
+};
+
+const quantity = (req, res, next) => {
+  const { quantity } = req.body;
+  if (typeof quantity !== 'number') {
+    return res.status(HTTP_VALID_STATUS).json({
+      message: '"quantity" must be a number'
+    });
+  };
+  if (quantity <= MIN_QUANT_ZERO) {
+    return res.status(HTTP_VALID_STATUS).json({
+      mensage: '"quantity" must be larger than or equal to 1'
+    });
+  };
+
+  next();
+};
+
+module.exports = {
+  name,
+  quantity
+};
