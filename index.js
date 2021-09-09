d4842e58c2dd6638050f75edf233cae0fd482de1
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-09 16:48:09 -0300

    feat: defined routes for products and sales

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+const Products = require('./controllers/Products');
+const Sales = require('./controllers/Sales');
+const Validate = require('./middlewares/validate');
+
+const PORT = 3000;
+const app = express();
+app.use(BodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
+// Products
+app.post('/products', Validate.name, Validate.quantity, Products.create);
+app.get('/products', Products.getAll);
+app.get('/products/:id', Products.getById);
+app.delete('/products/:id', Products.remove);
+app.put('/products/:id', Validate.name, Validate.quantity, Products.update);
+
+// Sales
+app.get('/sales', Sales.getAll);
+app.get('/sales/:id', Sales.getById);
