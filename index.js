17e002c1811a5e7d4345fdfbc34218a6b0cd0e46
Micaela Alves <micaela.alves93@gmail.com>
2021-09-10 16:50:38 -0300

    script base app rodando

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
