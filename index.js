452680982979f0f6a8f33191d4b387949c8e3424
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-10 09:15:32 -0400

    add the basic in the index.js file

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`API escutando na porta ${PORT}`));
