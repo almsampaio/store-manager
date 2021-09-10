508f82fe0f971d90bcd7e0815c15de8a7fda040a
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-09-10 19:03:20 -0300

    Feat: Cria connectionDB com mongoDB e arquivo .env

---
+++ models/connectionDB.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const DB_NAME = 'StoreManager';
+
+let db = null;
+
+const connectionDB = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((con) => {
+    db = con.db(DB_NAME);
+    return db;
+  }));
+
+module.exports = connectionDB;
