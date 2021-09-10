6dcc47e7483bf68ce67565ec8ade2dfd00069404
Eduardo Seije <eduardoseije@gmail.com>
2021-09-10 15:06:17 -0300

    Complementação da estrutura do index.js, fazendo a importação do express e bodyparser, bem como determinando o app.listen na porta 3000

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online em:', PORT);
 });
