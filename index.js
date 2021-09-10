e01570099ee2f2423b258eb89b2e2bf28e5e3e8d
rogerio-lambert <rogerio.cl@gmail.com>
2021-09-10 16:55:21 -0300

    criaçao dos diretorios, rotas e connection

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsRouters = require('./routers/productsRouters');
+const salesRouters = require('./routers/salesRouters');
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouters);
+
+app.use('/sales', salesRouters);
+
+app.listen(PORT, () => console.log('RUN SERVER'));
---
+++ routers/salesRouters.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', () => {});
+
+router.get('/:id', () => {});
+
+router.put('/:id', () => {});
+
+router.post('/', () => {});
+
+router.delete('/:id', () => {});
+
+module.exports = router;
