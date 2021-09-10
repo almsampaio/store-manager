14133fbf5677c3eb25dc3363d4da1a9efa032654
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-10 15:55:52 -0400

    feat req 1 - Crie um endpoint para o cadastro de produtos

--- middlewares/productsMidd.js
+++ middlewares/productsMidd.js
@@ -5,6 +5,26 @@ const findDuplicated = async (name) => {
   return result;
 };
 
+const validateName = (name) => {
+  if (name.length < 5) {
+    return false;
+  }
+  return true;
+};
+
+const validateQuantity = (quantity) => {
+  if (quantity <= 0) return false;
+  return true;
+};
+
+const validateQuantityType = (quantity) => {
+  if (typeof quantity !== 'number') return false;
+  return true;
+};
+
 module.exports = {
   findDuplicated,
+  validateName,
+  validateQuantity,
+  validateQuantityType,
 };
--- services/productsService.js
+++ services/productsService.js
@@ -1,32 +1,14 @@
 const productModel = require('../models/productsModel');
 const productsMidd = require('../middlewares/productsMidd');
+const errorMsg = require('../returnMsg');
 
 const create = async (name, quantity) => {
   const newProduct = await productsMidd.findDuplicated(name);
 
-  if (name.length < 5) {
-    return { err: { err: { code: 'invalid_data',
-      message: '"name" length must be at least 5 characters long' },
-    } };
-  }
-
-  if (typeof quantity !== 'number') {
-    return { err: { err: { code: 'invalid_data',
-      message: '"quantity" must be a number' },
-    } };
-  }
-
-  if (quantity <= 0) {
-    return { err: { err: { code: 'invalid_data',
-      message: '"quantity" must be larger than or equal to 1' },
-    } };
-  }
-
-  if (newProduct.length > 0) {
-    return { err: { err: { code: 'invalid_data',
-      message: 'Product already exists' },
-    } };
-  }
+  if (!productsMidd.validateName(name)) return errorMsg.validateNameMsg;
+  if (!productsMidd.validateQuantityType(quantity)) return errorMsg.invalidQtdTypeMsg;
+  if (!productsMidd.validateQuantity(quantity)) return errorMsg.invalidQtdNumberMsg;
+  if (newProduct.length > 0) return errorMsg.duplicatedProductMsg;
 
   const result = await productModel.create(name, quantity);
   return result;
