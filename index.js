f3c16b5c1bd75aaf769f56916d558237798259b8
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-10 17:19:23 -0300

    feat: importa bibliotecas importantes no index

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3000, () => { console.log('Ouvindo na porta 3000'); });
