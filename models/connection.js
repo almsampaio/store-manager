5a1c01f599c619f8613d5093ad31def400569010
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-10 17:52:05 -0300

    fix:connection

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
