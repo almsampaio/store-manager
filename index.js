334396c9c2b2c1c2aa063759db0a236b2fc84c7f
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-10 16:23:37 -0300

    index initial structure with routers

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const productsRouter = require('./routers/productsRouter');
+
+app.use('/products', productsRouter);
+
+const salesRouter = require('./routers/productsRouter');
+
+app.use('/sales', salesRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`);
 });
