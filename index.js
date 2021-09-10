4d980767cea992ca986d1f67989806bedeb4c900
Lucas Pedroso <lepsk8@gmail.com>
2021-09-10 12:48:20 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+require('./controllers/productController')(app);
+
+const PORT_DEFAULT = 3000;
+const PORT = process.env.PORT || PORT_DEFAULT;
+
+app.listen(PORT, () => {
+  console.log('Servidor Online');
 });
