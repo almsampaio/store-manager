34dcd6dfe543522deb5d4ee099769ceb9fdaa4b5
iago <iago.pferreiravr@gmail.com>
2021-09-10 18:02:57 -0300

    Cria o arquivo de mensagens de erro

---
+++ errors/messages.js
@@ -0,0 +1,16 @@
+const invaliData = 'invalid_data';
+
+const codes = {
+  invaliData: 'invalid_data',
+};
+
+const messages = {
+  productAlreadyExists: 'Product already exists',
+  productNameLength: '"name" length must be at least 5 characters long',
+};
+
+module.exports = {
+  invaliData,
+  codes,
+  messages,
+};
