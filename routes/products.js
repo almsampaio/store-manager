106a8e96c5fec99f39b5cbca1864405793f9206f
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 17:20:23 -0300

    feat: adiciona o middleware de erro na rota

--- routes/products.js
+++ routes/products.js
@@ -3,11 +3,14 @@ const express = require('express');
 const productsRouter = express.Router();
 const productsMiddlewares = require('../middlewares/products');
 const productsControllers = require('../controllers/productsControllers');
+const errorMiddleware = require('../middlewares/error');
 
 productsRouter.post(
   '/',
   productsMiddlewares.createProductValidation,
   productsControllers.createProduct,
 );
+
+productsRouter.use(errorMiddleware);
 
 module.exports = productsRouter;
