a369179b17d2ebe6f4e1f6a5d3dfbe318804f9b3
Joao Nasc <oi@joaonasc.dev>
2021-09-10 18:10:21 -0300

    feat: get product by name

--- models/Products.js
+++ models/Products.js
@@ -6,4 +6,12 @@ module.exports = {
     const product = await db.collection('product').insertOne({ name, quantity });
     return { id: product.insertedId, name, quantity };
   },
+
+  get: {
+    async byName(value) {
+      const db = await connect();
+      const product = await db.collection('product').findOne({ name: value });
+      return product;
+    },
+  },
 };
