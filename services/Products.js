57e9dda17d1ad94ada48d921341dbea6f73b46fb
Joao Nasc <oi@joaonasc.dev>
2021-09-10 18:30:18 -0300

    feat: validate quantity/min value

--- services/Products.js
+++ services/Products.js
@@ -4,6 +4,7 @@ const validate = require('../lib/validation');
 module.exports = {
   async create(name, quantity) {
     const foundProduct = await productsModel.get.byName(name);
+    validate.minValue({ quantity }, 1);
     validate.minLength(name, 5).textField('name');
     validate.isUnique(foundProduct, 'Product');
     const product = await productsModel.create(name, quantity);
