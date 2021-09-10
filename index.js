f1b2950916ecfcb6e408a9cf10a4b4ac52265e6f
Lucas Lara <lucasml@live.com>
2021-09-10 09:32:57 -0300

    Configuraçao Inicial o Index

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, ()=> console.log(`Server iniciado na porta ${PORT}`));
