80a31e0b0d494dba12a084acc7c231402a22b1fe
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 14:57:15 -0300

    feat: cria primeira estrutura do index.js

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(port, () => console.log('partiu'));
