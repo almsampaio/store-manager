e652d772fdc9ec84e14a60edaf084368df6e0212
lucas-paz <lucas.hpaz@outlook.com>
2021-09-10 19:20:03 -0300

    cria os services getById e getAll

--- services/products.js
+++ services/products.js
@@ -10,7 +10,23 @@ const addNew = async (newProductObj) => {
   return newProduct;
 };
 
+const getAll = async () => productModel.getAll();
+
+const getById = async (id) => {
+  const result = await productModel.getById(id);
+
+  if (result.message && result.message === 'id invalido') {
+    return builtError(422, 'invalid_data', 'Wrong id format');
+  }
+  if (result.message && result.message === 'nao encontrado') {
+    return builtError(404, 'Not found', 'Any element found with this id');
+  }
+  return result;
+};
+
 module.exports = {
   addNew,
   builtError,
+  getAll,
+  getById,
 };
