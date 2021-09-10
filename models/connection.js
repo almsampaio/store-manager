9207f1f9fd231ea67bb34e2045df28eaa2f3357a
Hernanne Domiciano <hernannedomiciano@gmail.com>
2021-09-10 18:28:42 -0300

    fix: req01

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
 let db = null;
 
