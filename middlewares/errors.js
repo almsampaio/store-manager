accf1bd8583f14aab52a50acac0d3fdd742ed112
lucas-paz <lucas.hpaz@outlook.com>
2021-09-10 15:20:05 -0300

    cria middleware de erros generico

---
+++ middlewares/errors.js
@@ -0,0 +1,17 @@
+const handleErrors = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(422).json({ err: {
+      code: 'Invalida_data',
+      message: err.details[0].message,
+    } });
+  }
+
+  return res.status(err.code).json({ err: {
+    code: err.codeType,
+    message: err.message,
+  } });
+};
+
+module.exports = {
+  handleErrors,
+};
