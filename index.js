6f108cea2c3e27fa984fb6463dbdba57c8a19fee
RenzoSev <sevilharenzo@gmail.com>
2021-09-10 14:16:04 -0300

    feat: index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const products = require('./routes/products');
+const sales = require('./routes/sales');
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', products);
+
+app.use('/sales', sales);
+
+app.listen(PORT, () => console.log('Server is running!'));
