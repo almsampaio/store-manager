ceace8c84b9a121606011ad638cd015dddcb0018
Flavio Franco <flavio.win@gmail.com>
2021-09-10 17:04:38 -0300

    refatorando validacoes

--- services/validations.js
+++ services/validations.js
@@ -15,26 +15,40 @@ const notValidId = () => {
   return errorMessage;
 };
 
-const validationToCreate = async (name, quantity) => {
+const validateName = (name) => {
   if (!regexName.test(name)) {
     const errorMessage = generateError(INVALID_DATA, CHARACTER_LONG);
     return { errorMessage };
   }
+};
 
+const validateAlreadyExists = async (name) => {
   if (await productsModel.getProductByName(name)) {
     const errorMessage = generateError(INVALID_DATA, PRODUCT_ALREADY_EXISTS);
     return { errorMessage };
   }
+};
 
+const verifyTypeQuantity = (quantity) => {
   if (typeof quantity !== 'number') {
     const errorMessage = generateError(INVALID_DATA, TYPE_MB_NUMBER);
     return { errorMessage };
   }
+};
 
+const verifyLengthQuantity = (quantity) => {
   if (quantity < 1) {
     const errorMessage = generateError(INVALID_DATA, QUANTITY_GT_1);
     return { errorMessage };
   }
+};
+
+const validationToCreate = async (name, quantity) => {
+  if (validateName(name)) return validateName(name);
+  const result = await validateAlreadyExists(name);
+  if (result) return result;
+  if (verifyTypeQuantity(quantity)) return verifyTypeQuantity(quantity);
+  if (verifyLengthQuantity(quantity)) return verifyLengthQuantity(quantity);
 };
 
 module.exports = {
