d34ceb31f50065b57332dc79dc52652b202bf7cc
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 17:19:01 -0300

    fix: as verificações do joi

--- middlewares/products.js
+++ middlewares/products.js
@@ -6,8 +6,8 @@ const createProductValidation = (req, _res, next) => {
   const schema = { name, quantity };
 
   const { error } = Joi.object({
-    name: Joi.string().min(6).required(),
-    quantity: Joi.number().min(1).required(),
+    name: Joi.string().min(5).required(),
+    quantity: Joi.number().integer().min(1).required(),
   }).validate(schema);
 
   if (error) return next(error);
