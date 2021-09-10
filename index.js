83d48cacdf4228d960a428b01eb1c4af4cf0be26
Hudson Carlos <hc88806@gmail.com>
2021-09-10 15:36:42 -0400

    starting project store manager

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const bodyParse = require('body-parser');
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParse.json());
+
+app.listen(port, () => console.log(port));
+
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
