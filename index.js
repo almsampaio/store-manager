cecbd15c0a75d7cd9e2cd115fe1b65dbb4376667
Maria Baeta <m.martinsdg@gmail.com>
2021-09-10 15:33:22 -0300

    feat: :sparkles: Configurando o ambiente de desenvolvimento

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`localHost ouvindo a porta ${PORT}`);
 });
---
+++ models/connection.js
@@ -0,0 +1,28 @@
+// models/connection.js
+
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+// Url sugerida para a execução do projeto:
+// Para rodar local:
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// Para subir para o Git:
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+let db = null;
+
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }));
+
+module.exports = connection;
