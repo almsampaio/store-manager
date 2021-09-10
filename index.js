6fbef95d9d196428fa54d5143f89ee1c9bc13a9c
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-09-10 16:21:48 -0300

    Express Working

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Server Working on ${PORT} `));
