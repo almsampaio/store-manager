27517dde0f7a180f6fac2543a78bf8e363526da3
Lucas Pedroso <lepsk8@gmail.com>
2021-09-10 12:49:39 -0300

    feat: add AppError

---
+++ controllers/validations/error.js
@@ -0,0 +1,18 @@
+const { StatusCodes } = require('http-status-codes');
+
+class AppError extends Error {
+  constructor(
+    message,
+    err = {},
+    codeStatus = StatusCodes.UNPROCESSABLE_ENTITY,
+    name = 'Error'
+  ) {
+    super();
+    this.message = message;
+    this.name = name;
+    this.err = err;
+    this.codeStatus = codeStatus;
+  }
+}
+
+module.exports = AppError;
