42a905b5b4667f413b7ad7cf83891d9a617f272f
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-09-10 14:53:03 -0300

    Add folders models, controllers, services and file connection

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
