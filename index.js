431e43cae8165a0496ae17813d40037a36b67e5f
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-09-10 17:11:42 -0300

    Criando a estrutuda da arquitetura MSC e configurando o nodemon

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Aplicação funcionando na porta ${PORT}`));
