f9952084dd0dc5cf8bf63cf210c34024864a7e2b
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 17:56:22 -0300

    fix: retorno de erro quando ja tem o produto cadastrado

--- services/productsServices.js
+++ services/productsServices.js
@@ -4,7 +4,7 @@ const createProduct = async ({ name, quantity }) => {
   const alreadyExists = await productsModels.getProductByName(name);
 
   if (alreadyExists) {
-    return { code: 422, message: 'Product already exists' };
+    return { code: 'invalid_data', type: 422, message: 'Product already exists' };
   }
 
   const response = await productsModels.createProduct({ name, quantity });
