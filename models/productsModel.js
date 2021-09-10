946ef4dbead319d9a2a06975bea31193a2c6a803
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-10 18:36:16 -0300

    product Model functions

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,18 +1,38 @@
 const mongoConnection = require('./connection');
 
-async function addProduct() {
-  console.log('adc produto');
-}
-
 async function getAll() {
   const db = await mongoConnection.getConnection();
-
   const products = await db.collection('products').find().toArray();
 
   return products;
 }
 
+async function getByName(name) {
+  const db = await mongoConnection.getConnection();
+  const productsByName = await db
+  .collection('products')
+  .find({ name }, {})
+  .toArray();
+
+  return productsByName;
+}
+
+async function addProduct({ name, quantity }) {
+  console.log('chegou no model');
+  const db = await mongoConnection.getConnection();
+  const { insertedId: _id } = await db.collection('products').insertOne(
+    { name, quantity },
+  );
+
+  return {
+    _id,
+    name,
+    quantity,
+  };
+}
+
 module.exports = {
   getAll,
+  getByName,
   addProduct,
 };
