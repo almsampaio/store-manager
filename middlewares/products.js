5b213c7e05e0af5f262a70c2d062290123808424
lucas-paz <lucas.hpaz@outlook.com>
2021-09-10 18:34:45 -0300

    fix: middleware de validacao nao passando o erro

--- middlewares/products.js
+++ middlewares/products.js
@@ -8,7 +8,7 @@ const schemaNewProduct = Joi.object({
 const validateNewProduct = (req, _res, next) => {
     const check = schemaNewProduct.validate(req.body);
 
-    if (check.error) next(check);
+    if (check.error) next(check.error);
     next();
 };
 
