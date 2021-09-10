4904473dd5be6ce2fab57e345588591e45e7fb91
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-09-10 10:25:59 -0300

    pastas criadas e index feito

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productController = require('./controllers/productController');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = '3000';
+
+app.post('/products', productController.create);
+
+app.listen(PORT, () => {
+  console.log('Servidor rodando');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
