2431dd2927599cbc52463e988b3114b76440a32c
Felipe Flores <felipefloresvendas@gmail.com>
2021-09-10 15:06:31 -0300

    added index and init connection with database

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+
+const app = express();
+const SERVER_PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.post('/products');
+
+app.listen(SERVER_PORT, () => console.log(`Servidor rodando na porta: ${SERVER_PORT}`));
