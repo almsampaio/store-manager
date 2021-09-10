cc787626c0ba46b374a78db5210e04dc8d7a5c7f
Joao Nasc <oi@joaonasc.dev>
2021-09-10 19:10:11 -0300

    fix: return of get.byName and id format

--- models/Products.js
+++ models/Products.js
@@ -1,17 +1,18 @@
+const { ObjectId } = require('mongodb');
 const connect = require('./connection');
 
 module.exports = {
   async create(name, quantity) {
     const db = await connect();
     const product = await db.collection('product').insertOne({ name, quantity });
-    return { id: product.insertedId, name, quantity };
+    return { id: ObjectId(product.id), name, quantity };
   },
 
   get: {
     async byName(value) {
       const db = await connect();
       const product = await db.collection('product').findOne({ name: value });
-      return product;
+      return { id: ObjectId(product.id), name: product.name, quantity: product.quantity };
     },
   },
 };
