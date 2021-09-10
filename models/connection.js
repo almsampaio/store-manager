5646a7c42c80af921086fdfd740062b0233c7415
Flavio Franco <flavio.win@gmail.com>
2021-09-10 14:38:39 -0300

    iniciando projeto, #VQV :rocket:

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGODB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGODB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
