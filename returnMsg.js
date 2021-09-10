2647a40a79455d03da0f1306eab8eedbe53267d4
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-10 15:54:42 -0400

    add returnMsg File with all error msg

---
+++ returnMsg.js
@@ -0,0 +1,22 @@
+const duplicatedProductMsg = { err: { err: { code: 'invalid_data',
+  message: 'Product already exists' } },
+};
+
+const validateNameMsg = { err: { err: { code: 'invalid_data',
+  message: '"name" length must be at least 5 characters long' } },
+};
+
+const invalidQtdNumberMsg = { err: { err: { code: 'invalid_data',
+    message: '"quantity" must be larger than or equal to 1' } },
+};
+
+const invalidQtdTypeMsg = { err: { err: { code: 'invalid_data',
+  message: '"quantity" must be a number' } },
+};
+
+module.exports = {
+  duplicatedProductMsg,
+  validateNameMsg,
+  invalidQtdNumberMsg,
+  invalidQtdTypeMsg,
+};
