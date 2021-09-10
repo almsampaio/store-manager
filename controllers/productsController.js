709da90e277a402367637f761e4596cfc53918fb
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-10 18:33:43 -0300

    getAll and addProducts for products controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,24 @@
 const productsService = require('../services/productsService');
 
-async function getAll() {
+async function getAll(_req, res) {
   const products = await productsService.getAll();
-  return products;
+  res.status(200).json(products);
+}
+
+async function addProduct(req, res) {
+  const { name, quantity } = req.body;
+  const addedProduct = await productsService.addProduct({
+    name,
+    quantity,
+  });
+
+  if (addedProduct.message) {
+    return res.status(422).json({ err: addedProduct });
+  }
+  res.status(201).json(addedProduct);
 }
 
 module.exports = {
   getAll,
+  addProduct,
 };
