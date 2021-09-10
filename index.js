503ff97a282c67bf24d4a38f9096e326812c6210
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-10 15:10:41 -0300

    folder organization

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+app.listen(PORT, () => console.log('Example app listening on port port!'));
