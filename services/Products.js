c9d069f370a0c699c196168755e0ca78f03fdff5
Joao Nasc <oi@joaonasc.dev>
2021-09-10 18:01:22 -0300

    feat: validate if name is at least 5 characters long

--- services/Products.js
+++ services/Products.js
@@ -1,9 +1,10 @@
 const productsModel = require('../models/Products');
+const validate = require('../lib/validation');
 
 module.exports = {
   async create(name, quantity) {
+    validate.minLength(name, 5).textField('name');
     const product = await productsModel.create(name, quantity);
-
     return product;
   },
 };
