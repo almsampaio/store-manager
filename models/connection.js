cafa13c1ae270d8a3601a7e9b74bcf440c8b6626
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 17:19:56 -0300

    fix: url do mongo para testar a api usando postman e o BD

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,8 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const DB_NAME = 'StoreManager';
 
 let schema = null;
 
@@ -11,7 +13,7 @@ async function getConnection() {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then((conn) => conn.db('model_example'))
+    .then((conn) => conn.db(DB_NAME))
     .then((dbSchema) => {
       schema = dbSchema;
       return schema;
