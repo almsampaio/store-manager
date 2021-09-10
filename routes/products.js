972eac059dc8c751608419fca299d87d0d02da05
Orlando Flores <orlando_flores@id.uff.br>
2021-09-10 16:17:02 -0300

    feat: chama o controller na route

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const productsRouter = express.Router();
 const productsMiddlewares = require('../middlewares/products');
+const productsControllers = require('../controllers/productsControllers');
 
-productsRouter.post('/', productsMiddlewares.createProductValidation, (_req, _res) => {
-
-});
+productsRouter.post(
+  '/',
+  productsMiddlewares.createProductValidation,
+  productsControllers.createProduct,
+);
 
 module.exports = productsRouter;
