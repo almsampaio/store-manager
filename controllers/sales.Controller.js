830b60fb49eb04bf81ea4f2b83fa96ebe179b3ec
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-11 20:13:21 -0300

    feat: funçao createSales na camada controlles com a validaçao do campo quantity quanto a valor

--- controllers/sales.Controller.js
+++ controllers/sales.Controller.js
@@ -1,7 +1,15 @@
-// const salesService = require('../services/salesService');
+const salesService = require('../services/salesService');
 
-const createSale = async (_req, _res) => {
-
+const createSale = async (req, res) => {
+  const saleArray = req.body;
+  const sale = await salesService.createSale(saleArray);
+  if (sale.err) {
+    const MESSAGE_ERROR_JSON = { err: sale.err };
+    if (sale.status === 422) {
+      const INVALID_NAME_STATUS_422 = 422;
+      return res.status(INVALID_NAME_STATUS_422).json(MESSAGE_ERROR_JSON);
+    }
+  }
  };
 
  module.exports = {
