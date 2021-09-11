f6e329f154e4d4a354832036509e7e45f2126c2a
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-11 19:21:02 -0300

    feat: controller products create test

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -1,23 +1,71 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+
+const ProductController = {
+  create: () => {}
+}
 
 describe('Ao chamar o controller de create - controller', () => {
   describe('quando o payload informado não é válido', () => {
-    it('é chamado o status com o código 422', () => {
-
+    const response = {};
+    const request = {};
+  
+    before(() => {
+      request.body = {
+        name: 'Produto Batista',
+        quantity: 'string',
+      };
+  
+      response.status = sinon.stub()
+        .returns(response);
+      response.json = sinon.stub()
+        .returns();
     });
 
-    it('é chamado o json com a mensagem "Dados inválidos"', () => {
+    it('é chamado o status com o código 422', async () => {
+      await ProductController.create(request, response);
 
+      expect(response.status.calledWith(422)).to.be.equal(true);
+    });
+
+    it('é chamado o json com o objeto "err"', async () => {
+      await ProductController.create(request, response);
+      const err = {
+        err: { code: 'invalid_data', message: '"quantity" must be a number' }
+      }
+
+      expect(response.json.calledWith(err)).to.be.equal(true);
     });
   });
 
   describe('quando é inserido com sucesso', () => {
-    it('é chamado o status com o código 201', () => {
+    const response = {};
+    const request = {};
 
+    before(() => {
+      request.body = {
+        name: 'Produto Batista',
+        quantity: 1,
+      };
+  
+      response.status = sinon.stub()
+        .returns(response);
+      response.json = sinon.stub()
+        .returns();
     });
 
-    it('é chamado o json com a mensagem "Filme criado com sucesso!"', () => {
+    it('é chamado o status com o código 201', async () => {
+      await ProductController.create(request, response);
 
+      expect(response.status.calledWith(201)).to.be.equal(true);
+    });
+
+    it('é chamado o json com os dados do produto criado', async () => {
+      await ProductController.create(request, response);
+      const product = { name: 'Produto Batista',
+      quantity: 1 };
+
+      expect(response.json.calledWith(product)).to.be.equal(true);
     });
   });
 });
