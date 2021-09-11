b7f19ddaee978223c8720e3bf5d83831a67d2bc1
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-11 16:31:40 -0300

    function addSales in sales controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -5,6 +5,17 @@ async function getAll() {
   return sales;
 }
 
+async function addSales(req, res) {
+  const { body: salesList } = req;
+
+  const addedSales = await salesService.addSales(salesList);
+
+  if (addedSales.message) return res.status(422).json({ err: addedSales });
+
+  res.status(200).json(addedSales);
+}
+
 module.exports = {
   getAll,
+  addSales,
 };
