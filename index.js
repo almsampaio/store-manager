1efdc75b1388b27876ce4f1c867625d93646cb09
mallmann02 <leomallmann020302@gmail.com>
2021-09-11 08:44:21 -0300

    feat(routes): create express instance and design first endpoint

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const productsController = require('./controllers/productsControler');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/products', productsController.create);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('Rodando a pau'));
