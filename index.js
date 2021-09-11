e6b271a8a536412cc2d664e2e141589597b8f01f
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-09-11 10:40:55 -0300

    starting project

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsRouter = require('./routers/productsRouter');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/products', productsRouter);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+
+const connection = () => {
+  return mongoClient
+    .connect(process.env.DB_URL, OPTIONS)
+    .then((conn) => conn.db(process.env.DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+   });
+};
+
+module.exports = connection;
