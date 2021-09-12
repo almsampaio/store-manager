125a193a1fd90b9873906fdc7dfc3a4677621159
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-09-12 14:44:14 -0300

    alter mongo url for remote test

--- models/connection.js
+++ models/connection.js
@@ -6,8 +6,10 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
+const MONGO_URL = process.env.DB_URL ||  'mongodb://mongodb:27017/StoreManager';
+
 const connection = () => MongoClient
-    .connect(process.env.DB_URL, OPTIONS)
+    .connect(MONGO_URL, OPTIONS)
     .then((conn) => conn.db(process.env.DB_NAME))
     .catch((err) => {
       console.error(err);
