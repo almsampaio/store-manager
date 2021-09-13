e7c52681b771ed5a9351d97e4f6e4a9f39eb8279
gabrieldeori <deori.pessoal@gmail.com>
2021-09-13 10:09:14 -0300

    Add MongoDBConnection

---
+++ Connections/MongoDBConnection.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+let db = null;
+
+function connection() {
+    return db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+    db = conn.db('StoreManager');
+    return db;
+    });
+}
+
+module.exports = connection;
