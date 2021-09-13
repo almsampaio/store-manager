1a6a9b789966e72d18ea4136a7aac48970feed02
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 16:50:20 -0300

    feat: cria a rota de cadastro de vendas, passando pela validação e o middleware de erro

--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
 const salesRouter = express.Router();
+const salesControllers = require('../controllers/salesControllers');
+const { createSaleValidation } = require('../middlewares/sales');
+const salesErrorMiddleware = require('../middlewares/salesErrors');
 
+salesRouter.post('/', createSaleValidation, salesControllers.createSale);
+
+salesRouter.use(salesErrorMiddleware);
 module.exports = salesRouter;
