78d23a50fadb636b8457add48cad0a2a6828974e
rcalvs <rafaelcalvette@gmail.com>
2021-09-13 13:42:26 -0300

    Setup Inical, Connection e Index

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = '3000';
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}!`);
 });
