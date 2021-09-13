4c986c5968c55e6e7f521c817259a98dc5cbc171
phlopes <phlopes93@gmail.com>
2021-09-13 15:25:01 -0300

    creating connection with mongoDB and MSC structure

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('Project Store Manager'));
