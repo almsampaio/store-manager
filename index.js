f69a540e98e1a4a5d247d33faacc08ab68c5f21f
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-13 18:19:56 -0300

    Setup initial do app

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.post('/products', (_req, _res) => {
+
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
 });
