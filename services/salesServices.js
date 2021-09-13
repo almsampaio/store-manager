2a10f51659efbd0bf8a430aaa89199f811b2aebc
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 16:49:05 -0300

    fix: serviço para cadastrar uma venda

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,19 +1,6 @@
 const salesModels = require('../models/salesModels');
-const productsServices = require('./productsServices');
-
-const verifyProductsId = async (sales) => {
-  sales.forEach(async (sale) => {
-    const response = await productsServices.getProductById(sale.productId);
-    if (response.message) return response;
-  });
-  return true;
-};
 
 const createSale = async (sales) => {
-  const response = await verifyProductsId(sales);
-
-  if (response.message) return response;
-
   const sale = await salesModels.createSale(sales);
   return sale;
 };
