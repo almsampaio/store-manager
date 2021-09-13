ef1b248f593418941499fd5ab7aeb3fefbead9e0
igson <igson660@gmail.com>
2021-09-13 15:09:05 -0500

    connection created

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`On-line na porta ${PORT}`));
