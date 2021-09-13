6d00ab20837a4c81e5b67c564c3c3975df95d29c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-09-13 20:11:51 -0300

    Feat: cria arquivo products no controllers e funcoes getAll, findById e create

---
+++ controllers/products.js
@@ -0,0 +1,36 @@
+const productsService = require('../services/products');
+
+const HTTP_OK_STATUS = 200;
+const UNPROCESSABLE_ENTITY = 422;
+const HTTP_CREATED_STATUS = 201;
+
+const getAll = async (_req, res) => {
+  const getAllProducts = await productsService.getAll();
+  return res.status(HTTP_OK_STATUS).json({ products: getAllProducts });
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const findByIdProduct = await productsService.findById(id);
+  if (!findByIdProduct) {
+    return res.status(UNPROCESSABLE_ENTITY).json({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong id format',
+        },
+    });
+  }
+  return res.status(HTTP_OK_STATUS).json(findByIdProduct);
+};
+
+const create = async (req, res) => {
+  const { name, quantity } = req.body;
+  const createNewProduct = await productsService.create(name, quantity);
+  return res.status(HTTP_CREATED_STATUS).json(createNewProduct);
+};
+
+module.exports = {
+  getAll,
+  findById,
+  create,
+};
