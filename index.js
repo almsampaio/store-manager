00d25d4523ccae2f8ec3d8b46ef847f300f2e167
gabrieldeori <deori.pessoal@gmail.com>
2021-09-13 10:05:26 -0300

    Adicionando rota de testes e rota de produtos

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ProductsControler } = require('./Controllers/ProductsController');
+
+const app = express();
+
+const PORT = 3000;
+
+// process.stdout.write('\033c');
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-// start
+app.get('/test', (_request, response) => {
+  response.status(200).send('TÁ FUNCIONANDO');
+});
+
+app.post('/products', ProductsControler);
+
+app.listen(PORT, () => {
+  console.log(`Conexão aberta na porta ${PORT}`);
+});
