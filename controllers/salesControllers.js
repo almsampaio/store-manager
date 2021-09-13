14f0635e6cf4c7823d18581ebffe3984a1d538c3
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 20:00:51 -0300

    fix: parametro nao utilizado

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -9,7 +9,7 @@ const createSale = async (req, res, next) => {
   return res.status(200).json(response);
 };
 
-const getAllSales = async (req, res) => {
+const getAllSales = async (_req, res) => {
   const sales = await salesServices.getAllSales();
   return res.status(200).json(sales);
 };
