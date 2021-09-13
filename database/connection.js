39763ccb8836a15b73b014b22221cfe0f87f0d2c
Lucas Pedroso <lepsk8@gmail.com>
2021-09-13 15:28:44 -0300

    fix: add process.env.MONGO_DB_URL

--- database/connection.js
+++ database/connection.js
@@ -1,28 +1,21 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
-// For localhost tests
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-
-// For Evaluator Job Trybe
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
 
 let db = null;
 
-const connection = () => {
-  return db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-      .then((conn) => {
-        db = conn.db(DB_NAME);
-        return db;
-      });
-};
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }));
 
 module.exports = connection;
