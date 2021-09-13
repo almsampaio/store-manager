9fd794b1f1b7d8dd09f311d0a2569c5cdb676cf8
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-13 18:26:09 -0300

    Cria conexao com o banco(MongoDB)

---
+++ models/connection.js
@@ -0,0 +1,26 @@
+const mongodb = require('mongodb');
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+function connection() {
+  return db
+    ? Promise.resolve(db)
+    : mongodb.MongoClient.connect(MONGO_DB_URL, OPTIONS)
+      .then((conn) => {
+        db = conn.db('StoreManager');
+        return db;
+      })
+      .catch((err) => {
+        console.log(err);
+        process.exit(1);
+      });
+}
+
+module.exports = connection;
