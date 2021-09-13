b2103f367244b9274ec11f7b894bece707581707
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 15:35:21 -0300

    feat: cria o modelo para cadastro de vendas

---
+++ models/salesModels.js
@@ -0,0 +1,15 @@
+const { getConnection } = require('./connection');
+
+const createSale = async (sales) => {
+  const db = await getConnection();
+  const { insertedId: _id } = await db.collection('sales').insertOne(sales);
+
+  return {
+    _id,
+    itensSold: sales,
+  };
+};
+
+module.exports = {
+  createSale,
+};
