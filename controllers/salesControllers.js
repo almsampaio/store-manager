28c0188e980093536ee06127a35f604357cc3a4d
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 17:20:02 -0300

    feat: cria controller para recuperar todas as vendas

--- controllers/salesControllers.js
+++ controllers/salesControllers.js
@@ -9,6 +9,12 @@ const createSale = async (req, res, next) => {
   return res.status(200).json(response);
 };
 
+const getAllSales = async (req, res) => {
+  const sales = await salesServices.getAllSales();
+  return res.status(200).json(sales);
+};
+
 module.exports = {
   createSale,
+  getAllSales,
 };
