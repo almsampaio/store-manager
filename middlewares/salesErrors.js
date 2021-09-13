802047470fc7c8b72be98517c8b69d1369c54c1a
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 16:59:16 -0300

    fix: adicionar outros erros

--- middlewares/salesErrors.js
+++ middlewares/salesErrors.js
@@ -1,5 +1,10 @@
-const errorMiddleware = (_err, _req, res, _next) => res.status(422).json(
-  { err: { code: 'invalid_data', message: 'Wrong product ID or invalid quantity' } },
-);
+const errorMiddleware = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    return res.status(422).json(
+      { err: { code: 'invalid_data', message: 'Wrong product ID or invalid quantity' } },
+    );
+  }
+  return res.status(err.type).json({ err: { code: err.code, message: err.message } });
+};
 
 module.exports = errorMiddleware;
