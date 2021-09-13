1c19f6cfc196f4fadc55480e411bcf3254f574a8
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-09-13 16:54:17 -0300

    Criado arquivo de conexão com mongo

---
+++ model/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+const { parsed } = require('dotenv').config();
+
+const MONGO_DB_URL = parsed.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+const DB_NAME = 'StoreManager';
+
+const connection = () => MongoClient
+  .connect(MONGO_DB_URL, OPTIONS)
+  .then((connect) => connect.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+module.exports = connection;
+
+console.log(MONGO_DB_URL);
