ab54ed37eab865b8db463d3727a82e69bcfd438c
lucas-paz <lucas.hpaz@outlook.com>
2021-09-13 16:11:25 -0300

    cria middleware de validações

--- Routes/sales.js
+++ Routes/sales.js
@@ -1,7 +1,9 @@
 const express = require('express');
-// const salesController = require('../controllers/sales');
+const salesController = require('../controllers/sales');
 // const {  } = require('../middlewares/sales');
 
 const router = express.Router();
+
+router.post('/', salesController.addNew);
 
 module.exports = router;
--- middlewares/sales.js
+++ middlewares/sales.js
@@ -1,15 +1,17 @@
 const Joi = require('joi');
 
-const schemaNewProduct = Joi.object({
+const schemaNewSale = Joi.object({
+  productId: Joi.string().required(),
+  quantity: Joi.number().integer().min(1).required(),
 });
 
-const validateNewProduct = (req, _res, next) => {
-    const check = schemaNewProduct.validate(req.body);
+const validateNewSale = (req, _res, next) => {
+    const check = schemaNewSale.validate(req.body);
 
     if (check.error) next(check.error);
     next();
 };
 
 module.exports = {
-  validateNewProduct,
+  validateNewSale,
 };
