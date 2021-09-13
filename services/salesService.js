9c9ab27e1ee7a8dc48ddc3d9ed3afe56f0f3c840
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-13 18:40:15 -0300

    adding stock validations to sales functions

--- services/salesService.js
+++ services/salesService.js
@@ -4,8 +4,11 @@ const productsModel = require('../models/productsModel');
 const invalidErrorMsg = 'Wrong product ID or invalid quantity';
 const notFoundErrorMsg = 'Sale not found';
 const invalidSaleErrorMsg = 'Wrong sale ID format';
+const stockProblemMsg = 'Such amount is not permitted to sell';
+
 const invalidCode = 'invalid_data';
 const notFoundCode = 'not_found';
+const stockCode = 'stock_problem';
 
 function quantityIsValid(quantity) {
   const isNumber = typeof quantity === 'number';
@@ -74,6 +77,10 @@ async function addSales(salesList) {
   );
   if (!isSaleProductValid.every((e) => e)) return { code: invalidCode, message: invalidErrorMsg };
 
+  // update stock
+  const updateStockResponse = await updateStock(salesList);
+  if (updateStockResponse.message) return updateStockResponse;
+
   const addedSales = await salesModel.addSales(salesList);
   return addedSales;
 }
@@ -88,6 +95,13 @@ async function updateSales({ id, productId, quantity }) {
   const validSale = await getById(id);
   if (validSale.message) return { code: invalidCode, message: invalidErrorMsg };
 
+  const currentStock = await checkProductStock(productId);
+  const currentSaleQty = validSale.itensSold.find((i) => i.productId === productId).quantity;
+  const newStockQty = currentStock + currentSaleQty - quantity;
+
+  if (newStockQty < 0) return { code: stockCode, message: stockProblemMsg };
+  await productsModel.updateProductQty(productId, newStockQty);
+
   const updatedSales = await salesModel.updateSales({ id, productId, quantity });
   return updatedSales;
 }
@@ -96,6 +110,12 @@ async function deleteSales(id) {
   const validSale = await getById(id);
 
   if (validSale.message) return { code: invalidCode, message: invalidSaleErrorMsg };
+
+  validSale.itensSold.forEach(async (i) => {
+    const currentStock = await checkProductStock(i.productId);
+    const newStockQty = currentStock + i.quantity;
+    await productsModel.updateProductQty(i.productId, newStockQty);
+  });
 
   await salesModel.deleteSales(id);
   return validSale;
