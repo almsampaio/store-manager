ddedcd729e5ba5d36b4c2da5de636d3210fd5b70
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-09-13 19:30:56 -0300

    Feat: cria arquivo products no models e funcoes getAll, findById e create

---
+++ models/Products.js
@@ -0,0 +1,31 @@
+const { ObjectId } = require('mongodb');
+const connectionDB = require('./connectionDB');
+
+const getAll = async () => {
+  const getAllProducts = await connectionDB()
+    .then((db) => db.collection('products').find({}).toArray());
+
+  return getAllProducts;
+};
+
+const findById = async (id) => {
+  const productsData = await connectionDB()
+    .then((db) => db.collection('products').findOne(ObjectId(id)));
+  
+  if (!productsData) return null;
+  
+  return productsData;
+};
+
+const create = async (name, quantity) => {
+  const newProduct = await connectionDB()
+    .then((db) => db.collection('products').insertOne({ name, quantity }));
+  
+  return newProduct;
+};
+
+module.exports = {
+  getAll,
+  findById,
+  create,
+};
