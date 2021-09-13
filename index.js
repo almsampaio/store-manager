ccd84ee6e7cd699f86809ac75da55639fc911859
Tiago Santos <tiago881@gmail.com>
2021-09-13 17:00:24 -0300

    fix: lint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
+// app.get('/', (_request, response) => {
+//   response.send();
+// });
