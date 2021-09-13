aadd8f770a9f9762c7186cb4735372c30d48a304
hannibal1207 <mathausannibal@gmail.com>
2021-09-13 19:12:02 -0300

    correço lint

--- model/connection.js
+++ model/connection.js
@@ -15,14 +15,12 @@ const OPTIONS = {
 
 let db = null;
 
-const connection = () => {
-  return db
+const connection = () => (db
     ? Promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
       .then((conn) => {
         db = conn.db(DB_NAME);
         return db;
-      });
-};
+      }));
 
 module.exports = connection;
