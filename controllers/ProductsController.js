55e9c272ebaf62a8a28ff822b387cfe385464877
Rodrigo Rech <rodrigorech92@gmail.com>
2021-09-13 17:58:47 -0300

    Feat: ProductsController.create implemented

---
+++ controllers/ProductsController.js
@@ -0,0 +1,17 @@
+const ProductsService = require('../services/productService');
+
+const create = async (req, res) => {
+  const { name, quantity } = req.body;
+
+  const product = await ProductsService.create({ name, quantity });
+
+  if (!product.id) {
+    return res.status(422).json(product);
+  }
+
+  return res.status(201).json(product);
+};
+
+module.exports = {
+  create,
+};
