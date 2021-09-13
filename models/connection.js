3fede66c3613e9fc197c498d85a3dae2afa43195
lucas-paz <lucas.hpaz@outlook.com>
2021-09-13 18:35:41 -0300

    altera url

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let schema = null;
