2572980b445a0ee8454d382f4cb6aabc80380398
Orlando Flores <orlando_flores@id.uff.br>
2021-09-13 17:27:49 -0300

    feat: cria servide para recuperar uma venda especifica

--- services/salesServices.js
+++ services/salesServices.js
@@ -10,7 +10,16 @@ const getAllSales = async () => {
   return sales;
 };
 
+const getSaleById = async (id) => {
+  const sale = await salesModels.getSaleById(id);
+
+  if (!sale) return { code: 'not_found', type: 404, message: 'Sale not found' };
+
+  return sale;
+};
+
 module.exports = {
   createSale,
   getAllSales,
+  getSaleById,
 };
