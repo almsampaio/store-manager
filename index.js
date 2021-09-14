bfc28d662d841e02f6c0620f455bed6622b376e6
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-14 10:09:13 -0300

    Configured index.js to start the API and using some packages to help with some functions

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
 });
