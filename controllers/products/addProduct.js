9c7b2168f2f9dc2d2f59f784f0f8f11a3e261927
Sergio Martins <sergiomos@pop-os.localdomain>
2021-09-14 15:44:42 -0300

    Fix: get All products

--- controllers/products/addProduct.js
+++ controllers/products/addProduct.js
@@ -1,7 +1,6 @@
 const { create } = require('../../models/Products'); 
-
 const { created } = require('../../utils/httpStatus');
-//
+
 const addProduct = async (req, res) => {
   const { name, quantity } = req.body;
 
--- controllers/products/getAll.js
+++ controllers/products/getAll.js
@@ -4,7 +4,7 @@ const { ok } = require('../../utils/httpStatus');
 const getAll = async (_req, res) => {
   const products = await productsModel.getAll();
 
-  res.status(ok).json(products);
+  res.status(ok).json({ products });
 };
 
 module.exports = getAll;
