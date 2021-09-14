1ee78783785694d44fcd72b009d14382936af6a9
Sérgio Martins <sergiomos2010@gmail.com>
2021-09-14 05:47:47 -0300

    Fix: error middleware message key

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,7 +4,8 @@ const errorMiddleware = (err, _req, res, _next) => {
   const { message } = err;
   const newMessage = message.replace(/greater/, 'larger');
 
-  return res.status(unprocessableEntity).json({ err: { code: 'invalid_data', newMessage } });
+  return res.status(unprocessableEntity)
+    .json({ err: { code: 'invalid_data', message: newMessage } });
 };
 
 module.exports = errorMiddleware;
