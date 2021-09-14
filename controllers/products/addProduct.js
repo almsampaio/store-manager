45015232b232bc3b1defb7c0c83cb923aaff6368
Sergio Martins <sergiomos@pop-os.localdomain>
2021-09-14 14:38:21 -0300

    Test

--- controllers/products/addProduct.js
+++ controllers/products/addProduct.js
@@ -6,6 +6,8 @@ const addProduct = async (req, res) => {
   const { name, quantity } = req.body;
 
   const createdProduct = await create({ name, quantity });
+
+  console.log(createdProduct);
   
   res.status(created).json(createdProduct);
 };
