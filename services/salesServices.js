73ac787fe0f22c7b20f6e0f2a8c4ee3f1f41e14c
Orlando Flores <orlando_flores@id.uff.br>
2021-09-14 09:52:08 -0300

    feat: cria verificações de estoque para efetuar uma venda

--- services/salesServices.js
+++ services/salesServices.js
@@ -1,7 +1,36 @@
 const salesModels = require('../models/salesModels');
+const productsModels = require('../models/productsModels');
+
+const getProductQuantity = async (id) => {
+  const { quantity } = productsModels.getProductById(id);
+  return quantity;
+};
+
+const verifiesSalesAmount = async (amount, id) => {
+  const totalQuantity = await getProductQuantity(id);
+  if (totalQuantity - amount <= 0) {
+    return false;
+  }
+  return true;
+};
+
+const updateSoldProducts = async (sales) => {
+  sales.forEach(async ({ productId, quantity }) => {
+    if (!verifiesSalesAmount(quantity, productId)) {
+      return false;
+    }
+    await productsModels.updateSoldProduct(productId, quantity);
+  });
+};
 
 const createSale = async (sales) => {
   const sale = await salesModels.createSale(sales);
+
+  const verifyAmount = await updateSoldProducts(sales);
+  if (!verifyAmount) {
+    return { code: 'stock_problem', type: 404, message: 'Such amount is not permitted to sell' };
+  }
+
   return sale;
 };
 
