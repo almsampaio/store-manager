acbbddc30633b065e8875c331a988981dec712aa
Sérgio Martins <sergiomos2010@gmail.com>
2021-09-14 03:56:50 -0300

    create addProduct model

--- controllers/products/addProduct.js
+++ controllers/products/addProduct.js
@@ -1,9 +1,13 @@
+const { create } = require('../../models/Products'); 
+
 const { created } = require('../../utils/httpStatus');
 
 const addProduct = async (req, res) => {
   const { name, quantity } = req.body;
 
-  res.status(created).json({ name, quantity });
+  const newProduct = await create({ name, quantity });
+
+  res.status(created).json(newProduct);
 };
 
 module.exports = addProduct;
---
+++ models/Products.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const COLLECTION_NAME = 'products';
+
+exports.create = async ({ name, quantity }) => {
+  const db = await connection();
+  const newProduct = await db.collection(COLLECTION_NAME).insertOne({ name, quantity });
+
+  return newProduct;
+};
