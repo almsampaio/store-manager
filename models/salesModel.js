7f99486269ccd650dc74dc3a4408dbbea199aa15
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-14 17:29:38 -0300

    fix 5: now create the entry on database

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,8 +1,8 @@
 const connection = require('./connection');
 
 const create = async (sale) => {
-  const newSale = connection()
-    .then((db) => db.collection('sales').insertOne(sale));
+ const newSale = await connection()
+    .then((db) => db.collection('sales').insertOne({ sale }));
 
   return {
     _id: newSale.insertedId,
