6344d3b6a9474d7bd04e1a09821f41f634ee0590
Patrick Morais <ppternunes@gmail.com>
2021-09-14 08:35:26 -0300

    fix connection

--- models/connection.js
+++ models/connection.js
@@ -5,7 +5,7 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 let db = null;
 
@@ -13,7 +13,7 @@ const connection = () => (db
     ? Promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS)
     .then((conn) => {
-      db = conn.db('model_example');
+      db = conn.db('StoreManager');
       return db;
     }));
 
