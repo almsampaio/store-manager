15fdd1b9b423791660eb44ee207a46856a95d2a4
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-09-14 14:36:13 -0300

    feature: criaçao de variavel de ambiente para nao ter que ficar trocando de url

--- models/connection.js
+++ models/connection.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
@@ -5,8 +6,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-// Para o github
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// Solução para não ficar trocando de URL para testes locais e github - créditos: Renatão e Corujão https://trybecourse.slack.com/archives/C01L16B9XC7/p1631296337060200
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
 // Para o BD local
 // const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
