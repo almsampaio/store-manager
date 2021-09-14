2f9b0879e75e83b132282281268e37f90463bab3
Joao Nasc <oi@joaonasc.dev>
2021-09-14 16:57:43 -0300

    feat: routes to get products

--- routes/Products.js
+++ routes/Products.js
@@ -4,6 +4,8 @@ const productController = require('../controllers/Products');
 
 const routes = express.Router();
 
+routes.get('/', productController.get);
 routes.post('/', productController.create);
+routes.get('/:id', productController.get);
 
 module.exports = routes;
