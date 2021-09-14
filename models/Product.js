489ef69488b08528c636b90ef60f2690af96bd28
Patrick Morais <ppternunes@gmail.com>
2021-09-14 09:45:53 -0300

    fix

--- models/Product.js
+++ models/Product.js
@@ -13,15 +13,12 @@ const create = async (name, quantity) =>
       create,
     };
 
-    const findByName = async (name, quantity) => {
-      // Determinamos se devemos buscar com ou sem o nome do meio
-      const query = { name, quantity };
-    
-      // Executamos a consulta e retornamos o resultado
+    const findByName = async (name) => {
+      const query = { name };
+
       const product = await connection().then((db) =>
         db.collection('products').findOne(query));
-    
-      // Caso nenhum author seja encontrado, devolvemos null
+
       if (!product) return null;
     
       return product;
--- services/Product.js
+++ services/Product.js
@@ -1,15 +1,16 @@
 const Product = require('../models/Product');
 
 const create = async (name, quantity) => {
-  const existingProduct = await Product.findByName(name, quantity);
+  const existingProduct = await Product.findByName(name);
 
   if (existingProduct) {
-    return {
-      errr: {
+    const errorMsg = {
+      err: {
         code: 'invalid_data',
         message: 'Product already exists',
       },
     };
+    return errorMsg;
   }
 
    return Product.create(name, quantity);
