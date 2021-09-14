14c806a1362891026af86d6928a434501a4ac00c
Rodrigo Rech <rodrigorech92@gmail.com>
2021-09-14 15:16:11 -0300

    Fix: error msgs

--- test/unit/services.js
+++ test/unit/services.js
@@ -19,7 +19,7 @@ describe('Insere um novo produto no Banco de dados', () => {
       const response = await ProductService.create(produto);
 
       expect(response).to.be.equal({ err: { code: 'invalid_data',
-      message: '"quantity" must be a integer number' } });
+      message: '"quantity" must be a number' } });
     });
   });
   describe('quando a quantidade eh invalida', () => {
@@ -39,7 +39,7 @@ describe('Insere um novo produto no Banco de dados', () => {
       const response = await ProductService.create(produto);
 
       expect(response).to.be.equal({ err: { code: 'invalid_data',
-      message: '"quantity" must be a integer number' } });
+      message: '"quantity" must be a number' } });
     });
     it('quando a quantidade eh menor ou igual a 0', async () => {
       const produto = {
@@ -69,7 +69,7 @@ describe('Insere um novo produto no Banco de dados', () => {
     const response = await ProductService.create(produto);
 
     expect(response).to.be.equal({ err: { code: 'invalid_data',
-    message: '"name"  must be a string' } });
+    message: '"name" must be a string' } });
     });
   
     it('quando o nome tem menos de 6 caracteres', async () => {
@@ -80,7 +80,7 @@ describe('Insere um novo produto no Banco de dados', () => {
     const response = await ProductService.create(produto);
 
     expect(response).to.be.equal({ err: { code: 'invalid_data',
-    message: '"name" must be at least 5 characters long' } });
+    message: '"name" length must be at least 5 characters long' } });
     });
     it('quando o produto ja existe', async () => {
       const produto = {
