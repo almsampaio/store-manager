fce67495c615d050ba85d3c927bc2bd9cf972229
Joao Nasc <oi@joaonasc.dev>
2021-09-14 17:21:41 -0300

    feat: route to update a product

--- routes/Products.js
+++ routes/Products.js
@@ -7,5 +7,6 @@ const routes = express.Router();
 routes.get('/', productController.get);
 routes.post('/', productController.create);
 routes.get('/:id', productController.get);
+routes.put('/:id', productController.update);
 
 module.exports = routes;
