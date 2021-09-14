988ac5fdd123216e987cd4ad213d426943442ca4
Sergio Martins <sergiomos@pop-os.localdomain>
2021-09-14 16:36:45 -0300

    req 3

--- controllers/products/updateProduct.js
+++ controllers/products/updateProduct.js
@@ -1,12 +1,13 @@
+const { ok } = require('../../utils/httpStatus');
 const productModel = require('../../models/Products');
 
-const updateProduct = (req, res) => {
+const updateProduct = async (req, res) => {
   const { id } = req.params;
   const { name, quantity } = req.body;
 
-  const updatedProduct = productModel.update(id, { name, quantity });
+  await productModel.update(id, { name, quantity });
 
-  res.status().json(updatedProduct);
+  res.status(ok).json({ _id: id, name, quantity });
 };
 
 module.exports = updateProduct;
