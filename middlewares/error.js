f9e22dbf7458dbe27df45e82f500e0426cf82f3a
Sérgio Martins <sergiomos2010@gmail.com>
2021-09-14 05:01:11 -0300

    Test

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,12 +1,10 @@
 const { unprocessableEntity } = require('../utils/httpStatus');
 
 const errorMiddleware = (err, _req, res, _next) => {
-  if (!err.code) {
-    const { message } = err;
-    const newMessage = message.replace(/greater/, 'larger');
+  const { message } = err;
+  const newMessage = message.replace(/greater/, 'larger');
 
-    return res.status(unprocessableEntity).json({ err: { code: 'invalid_data', newMessage } });
-  }
+  return res.status(unprocessableEntity).json({ err: { code: 'invalid_data', newMessage } });
 };
 
 module.exports = errorMiddleware;
