06b83c488ded5cc9b28aa2665fadf6b493362dda
Patrick Morais <ppternunes@gmail.com>
2021-09-14 07:22:44 -0300

    starting project

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
