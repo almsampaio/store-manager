da70d2239ae172055dedc691ed22ed91668e0274
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-14 11:25:55 -0300

    feat 1: created a Joi validation to the body requisitions on the Products Controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,27 @@
 const rescue = require('express-rescue');
+const Joi = require('joi');
+
 const service = require('../services/productsService');
 
-const create = rescue(async (req, res) => {
+const create = rescue(async (req, res, next) => {
+  const { error } = Joi.object({
+    product_name: Joi.string()
+      .min(5)
+      .not()
+      .empty()
+      .required(),
+
+    product_quantity: Joi.number()
+      .integer()
+      .min(1)
+      .required(),
+  })
+    .validate(req.body);
+
+  if (error) {
+    return next(error);
+  }
+
   const { product_name, product_quantity } = req.body;
 
   const createProduct = await service.create(product_name, product_quantity);
