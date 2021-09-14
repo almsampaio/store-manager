5d9180d0d48f793ba8f1331e612b1a9f68189ecb
defauth98 <neto.daniribeiro@gmail.com>
2021-09-14 15:49:53 -0300

    fix: requisito 01 mudando a url de conexao do mongo

--- src/model/connection.js
+++ src/model/connection.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const mongoConfig = {
+const MONGO_CONFIG = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 let schema = null;
 
@@ -13,8 +14,8 @@ module.exports = {
   getConnection() {
     if (schema) return Promise.resolve(schema);
     return MongoClient
-      .connect(MONGO_DB_URL, mongoConfig)
-      .then((conn) => conn.db('model_example'))
+      .connect(MONGO_DB_URL, MONGO_CONFIG)
+      .then((conn) => conn.db(DB_NAME))
       .then((dbSchema) => {
         schema = dbSchema;
         return schema;
