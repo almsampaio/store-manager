d0cd90d894b5f17f3ee879c10a4c63ebae93acef
Rodrigo Rech <rodrigorech92@gmail.com>
2021-09-14 09:19:08 -0300

    Fix lint

--- test/unit/services.js
+++ test/unit/services.js
@@ -19,7 +19,7 @@ describe('Insere um novo produto no Banco de dados', () => {
       const response = await ProductService.create(produto);
 
       expect(response).to.be.equal({ err: { code: 'invalid_data',
-      message: '"\quantity\" must be a integer number' } });
+      message: '"quantity" must be a integer number' } });
     });
   });
   describe('quando a quantidade eh invalida', () => {
