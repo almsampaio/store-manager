afbeebad2ee815ac1ac85082f4ec1ac81bf2ff68
gabrieldeori <deori.pessoal@gmail.com>
2021-09-14 14:31:09 -0300

    Adding extra validation to ObjectId

--- MODELS/ProductsModelMongo.js
+++ MODELS/ProductsModelMongo.js
@@ -37,6 +37,9 @@ async function findAllProductsInMongoDB() {
 }
 
 async function findOneProductInMongoDBByID(id) {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
   try {
     const db = await connection();
     const responseOfQuery = await db.collection('products').findOne(new ObjectId(id));
