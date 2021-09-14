a3841ba9300921d484bc780d6b99581d600e797e
Hudson Carlos <hc88806@gmail.com>
2021-09-14 19:13:42 -0400

    router '/product' testing

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParse = require('body-parser');
+const controllerProduct = require('./controller/products');
+
 const port = 3000;
 
 const app = express();
 
 app.use(bodyParse.json());
 
-app.listen(port, () => console.log(port));
+app.post('/products', controllerProduct.createProduct);
 
+app.use((err, _req, res, _next) => {
+  const { message, code, status } = err;
+  return res.status(status).json({ err: { code, message } });
+});
+
+app.listen(port, () => console.log(port));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
