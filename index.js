c676c73f994f4316c0e00ce2df6d3f92f593470f
jlfagundes <jlfagundes@gmail.com>
2021-09-15 10:54:39 -0300

    feat: initial structure basic API

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3001, () => console.log('API in running!'));
