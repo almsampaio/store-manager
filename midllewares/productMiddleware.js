22bc18568b3df80a03b23ee9b555001432f94fae
Tiago Santos <tiago881@gmail.com>
2021-09-15 18:04:19 -0300

    refactor: errosMsg

--- midllewares/productMiddleware.js
+++ midllewares/productMiddleware.js
@@ -1,37 +1,5 @@
 const productService = require('../services/productService');
-
-const messageErro = {
-  nameLength: {
-    err: {
-      code: 'invalid_data',
-      message: '"name" length must be at least 5 characters long',
-    },
-  },
-  nameExist: {
-    err: {
-      code: 'invalid_data',
-      message: 'Product already exists',
-    },
-  },
-  quantityMin: {
-    err: {
-      code: 'invalid_data',
-      message: '"quantity" must be larger than or equal to 1',
-    },
-  },
-  quantityNotNumber: {
-    err: {
-      code: 'invalid_data',
-      message: '"quantity" must be a number',
-    },
-  },
-  wrongIdFormat: {
-    err: {
-      code: 'invalid_data',
-      message: 'Wrong id format',
-    },
-  },
-};
+const messageErro = require('../utils/errosMsg');
 
 const validateName = (name, products) => {
   if (name.length < 5) return messageErro.nameLength;
