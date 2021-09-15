95c04f42462bc02d952d83df5cd2b13bdcb7f12e
Tiago Santos <tiago881@gmail.com>
2021-09-15 18:05:07 -0300

    feat: create object error

---
+++ utils/errosMsg.js
@@ -0,0 +1,34 @@
+const messageErro = {
+  nameLength: {
+    err: {
+      code: 'invalid_data',
+      message: '"name" length must be at least 5 characters long',
+    },
+  },
+  nameExist: {
+    err: {
+      code: 'invalid_data',
+      message: 'Product already exists',
+    },
+  },
+  quantityMin: {
+    err: {
+      code: 'invalid_data',
+      message: '"quantity" must be larger than or equal to 1',
+    },
+  },
+  quantityNotNumber: {
+    err: {
+      code: 'invalid_data',
+      message: '"quantity" must be a number',
+    },
+  },
+  wrongIdFormat: {
+    err: {
+      code: 'invalid_data',
+      message: 'Wrong id format',
+    },
+  },
+};
+
+module.exports = messageErro;
