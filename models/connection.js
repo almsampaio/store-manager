3603890954a038877652fb02c3738a68386e8242
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-15 12:42:38 -0300

    feat: req 5 - endpoint post sales

--- models/connection.js
+++ models/connection.js
@@ -5,8 +5,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/StoreManager';
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let db = null;
