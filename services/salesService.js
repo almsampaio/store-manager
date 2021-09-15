f133868026efab41cbbbe3f555e15be4a8b18c24
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-15 13:02:08 -0300

    feat 6: create in the Service layer getAll function

--- services/salesService.js
+++ services/salesService.js
@@ -2,6 +2,9 @@ const Sales = require('../models/salesModel');
 
 const create = async (sale) => Sales.create(sale);
 
+const getAll = async () => Sales.getAll();
+
 module.exports = {
   create,
+  getAll,
 };
