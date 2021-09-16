5a85b2881da40c1b0fd4cf5c7eb40234a0f3f2f8
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-09-16 18:26:51 -0300

    TESTE

--- models/connection.js
+++ models/connection.js
@@ -6,10 +6,10 @@ const OPTIONS = {
 };
 
 // Para rodar localmente
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+ const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // Para o avaliador funcionar
- const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
