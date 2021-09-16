4514a9dfda7cdcbda2a8320ff1a86a086a4da2d7
ScriptCamilo <script.camilo@gmail.com>
2021-09-16 15:09:42 -0300

    feat: main index updated

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const productsRouter = require('./routes/products');
+
+const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
+});
+
+app.use('/products', productsRouter);
+
+app.use((err, _req, res, _next) => {
+  const { status, code, message } = err;
+
+  res.status(status).json({
+    err: {
+      code,
+      message,
+    },
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
