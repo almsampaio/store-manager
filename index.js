f359882b43c78d17b1c361289d5a96e994b72922
Robertson <robertson.nunes@gmail.com>
2021-09-16 19:06:50 -0300

    Implementa Rotas

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const productController = require('./controllers/productController');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/products', productController.getAllProducts);
-app.get('/products/:id', productController.getProductById);
-app.post('/products', productController.createProduct);
-app.put('/products/:id', productController.updateProduct);
-app.delete('/products/:id', productController.deleteProduct);
+const productRouter = require('./routers/productRouter');
+
+app.use('/products', productRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
---
+++ routers/productRouter.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const productController = require('../controllers/productController');
+
+const router = express.Router();
+
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/', productController.createProduct);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
+
+module.exports = router; 
