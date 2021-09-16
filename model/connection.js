e5dead712072311e760deed8b94df9ebfff22cae
Luiz <luizfvaccarif@gmail.com>
2021-09-16 14:52:06 -0300

    Fix: .env config

--- model/connection.js
+++ model/connection.js
@@ -1,9 +1,9 @@
 const mongodb = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/StoreManager';
-
 // const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 module.exports = () =>
