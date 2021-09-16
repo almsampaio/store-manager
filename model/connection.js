b999baf294ff545c5547a6b8f4d5fb174ba84c41
hannibal1207 <mathausannibal@gmail.com>
2021-09-15 21:37:32 -0300

    muda para o link do avaliador

--- model/connection.js
+++ model/connection.js
@@ -2,9 +2,9 @@
 
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/StoreManager';
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
