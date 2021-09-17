b583608ae57ffb8d0e7f342aadd9d40d7f9d88fb
Rodrigo Rech <rodrigorech92@gmail.com>
2021-09-17 19:10:52 -0300

    Feat: ProductsController.deleteById implemented

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -43,9 +43,20 @@ const updateById = async (req, res) => {
   return res.status(200).json(product);
 };
 
+const deleteById = async (req, res) => {
+  const { _id } = req.params;
+
+  const deleteProduct = await ProductsService.deleteById(_id);
+  if (deleteProduct.err) {
+    return res.status(422).json(deleteProduct);
+  }
+  return res.status(200).json(deleteProduct);
+};
+
 module.exports = {
   create,
   getById,
   getAll,
   updateById,
+  deleteById,
 };
