62bad745aabce46d2513090d8de721269cf78575
rodolfo_oliveira <rod.vgo@gmail.com>
2021-09-16 21:35:53 -0300

    req 4

--- src/models/connection.js
+++ src/models/connection.js
@@ -11,14 +11,14 @@ const OPTIONS = {
 const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/StoreManager';
-// const { DB_NAME } = process.env;
+const DB_NAME = 'StoreManager';
 
 let db = null;
 
 const connection = () => (db
     ? Promise.resolve(db)
     : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
-        db = conn.db('StoreManager');
+        db = conn.db(DB_NAME);
         return db;
       }));
 
