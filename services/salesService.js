ef2753fb4eb65bb0c448ba6d6d7ffc705b710352
rogerio-lambert <rogerio.cl@gmail.com>
2021-09-17 00:16:31 -0300

    correçoes no codigo de atualizaçao de estoque

--- services/salesService.js
+++ services/salesService.js
@@ -1,55 +1,12 @@
 const salesModel = require('../models/salesModel');
 const productsService = require('./productsService');
 
-const updateProductQuantity = async (itensSold) => {
+const updateProductQuantity = async (itensSold, type) => {
   await itensSold.forEach(async (item) => {
     const { response: { name, quantity } } = await productsService.findById(item.productId);
-    await productsService.update(item.productId, name, (quantity - item.quantity));
+    const newQuatity = type === 'sub' ? (quantity - item.quantity) : (quantity + item.quantity);
+    await productsService.update(item.productId, name, newQuatity);
   });
-};
-
-const validationInfoGenerator = async (isValid, itensSold) => {
-  if (isValid.quantity && isValid.exists) {
-    await updateProductQuantity(itensSold);
-    return { isValid: true };
-  }
-  if (!isValid.exists) {
-    return { errorInfo: { err: {
-          code: 'invalid_data',
-          message: 'Wrong product ID or invalid quantity',
-        },
-      },
-      status: 422 };
-  }
-  return { errorInfo: { err: {
-          code: 'stock_problem',
-          message: 'Such amount is not permitted to sell',
-        },
-      },
-      status: 404 };
-};
-
-const isSaleValid = async (itensSold) => {
-  const isValid = await itensSold.reduce(async (acc, item) => {
-    const productSearched = await productsService.findById(item.productId);
-    if (productSearched.status === 422 || !Number.isInteger(item.quantity) || item.quantity < 1) {
-      return { ...acc, exists: false };
-    }
-    if ((productSearched.response.quantity - item.quantity) < 0) {
-      return { ...acc, quantity: false };
-    }
-    return acc;
-  }, { exists: true, quantity: true });
-
-  return validationInfoGenerator(isValid, itensSold);
-};
-
-const getAll = async () => {
-  const sales = await salesModel.getAll();
-  return {
-    response: { sales },
-    status: 200,
-  };
 };
 
 const findById = async (id) => {
@@ -71,6 +28,50 @@ const findById = async (id) => {
   };
 };
 
+const validationInfoGenerator = async (isValid, itensSold, id) => {
+  if (isValid.quantity && isValid.exists) {
+    if (id !== undefined) {
+      const { response } = await findById(id);
+      await updateProductQuantity(response.itensSold, 'add'); 
+    }
+    await updateProductQuantity(itensSold, 'sub');
+    return { isValid: true };
+  }
+  if (!isValid.exists) {
+    return { errorInfo: { err: { code: 'invalid_data',
+          message: 'Wrong product ID or invalid quantity',
+        } },
+        status: 422 };
+  }
+  return { errorInfo: { err: { code: 'stock_problem',
+          message: 'Such amount is not permitted to sell',
+        } },
+        status: 404 };
+};
+
+const isSaleValid = async (itensSold, id) => {
+  const isValid = await itensSold.reduce(async (acc, item) => {
+    const productSearched = await productsService.findById(item.productId);
+    if (productSearched.status === 422 || !Number.isInteger(item.quantity) || item.quantity < 1) {
+      return { ...acc, exists: false };
+    }
+    console.log('teste de quantidade', (productSearched.response.quantity - item.quantity));
+    if ((productSearched.response.quantity - item.quantity) < 0) {
+      return { ...acc, quantity: false };
+    }
+    return acc;
+  }, { exists: true, quantity: true });
+  return validationInfoGenerator(isValid, itensSold, id);
+};
+
+const getAll = async () => {
+  const sales = await salesModel.getAll();
+  return {
+    response: { sales },
+    status: 200,
+  };
+};
+
 const deleteById = async (id) => {
   const { status, response } = await findById(id);
   if (status === 404) {
@@ -81,6 +82,8 @@ const deleteById = async (id) => {
       status: 422,
     };
   }
+  const { response: { itensSold: oldItensSold } } = await findById(id);
+  await updateProductQuantity(oldItensSold, 'add'); 
   await salesModel.deleteById(id);
   return {
     response,
@@ -98,13 +101,13 @@ const create = async (itensSold) => {
 };
 
 const update = async (id, itensSold) => {
-  const { isValid, errorInfo, status: statusValidation } = await isSaleValid(itensSold);
-  if (!isValid) {
-    return { response: errorInfo, status: statusValidation };
-  }
   const { status, response } = await findById(id);
   if (status === 404) {
     return { response, status };
+  }
+  const { isValid, errorInfo, status: statusValidation } = await isSaleValid(itensSold, id);
+  if (!isValid) {
+    return { response: errorInfo, status: statusValidation };
   }
   const saleUpdated = await salesModel.update(id, itensSold);
   return { response: saleUpdated, status: 200 };
