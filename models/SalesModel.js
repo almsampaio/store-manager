d8c7be45ff84cfe08eb8b1af67f8b9c7e3c31337
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-17 19:21:41 -0300

    feat: implementation for model salesModel function update

--- models/SalesModel.js
+++ models/SalesModel.js
@@ -3,20 +3,6 @@ const mongoConnection = require('./connection');
 
 const COLLECTION_SALES = 'sales';
 
-const create = async (arraySold) => {
-  const salesCollection = await mongoConnection.getConnection()
-    .then((db) => db.collection(COLLECTION_SALES));
-  
-    const { insertedId: _id } = await salesCollection.insertOne({
-      itensSold: arraySold,
-    });
-
-    return {
-      _id,
-      itensSold: arraySold,
-    };
-};
-
 const getAll = async () => {
   const salesCollection = await mongoConnection.getConnection()
   .then((db) => db.collection(COLLECTION_SALES));
@@ -39,8 +25,40 @@ const getById = async (id) => {
   return sale;
 };
 
+const create = async (arraySold) => {
+  const salesCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection(COLLECTION_SALES));
+  
+    const { insertedId: _id } = await salesCollection.insertOne({
+      itensSold: arraySold,
+    });
+
+    return {
+      _id,
+      itensSold: arraySold,
+    };
+};
+
+const update = async (id, [saleNewData]) => {
+  if (!ObjectId.isValid(id)) return null;
+  const salesCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection(COLLECTION_SALES));
+
+  const updatedSale = await salesCollection.findOneAndUpdate(
+    { _id: new ObjectId(id) },
+    { $set: { itensSold: [saleNewData] } },
+    { 
+      upsert: true,
+      returnDocument: 'after',
+    },
+  );
+  
+  return updatedSale.value;
+};
+
 module.exports = {
   getById,
   getAll,
   create,
+  update,
 };
