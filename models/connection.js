f2277158330c9cdc0d2689e48f4b2c8eb5323b75
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-09-17 16:00:42 -0300

    correção connection

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb').MongoClient;
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 module.exports = () =>
