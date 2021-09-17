20c860047ad7b71cccae226f955f594c9af4333a
mallmann02 <leomallmann020302@gmail.com>
2021-09-16 22:39:26 -0300

    configure dotenv library

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
 const productsController = require('./controllers/productsControler');
@@ -23,4 +24,4 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(3000, () => console.log('Rodando certinho'));
+app.listen(process.env.PORT || 3000, () => console.log('Rodando certinho'));
