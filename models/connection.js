5f42b90fee60ecf29400772c03eb36a8ad86c64d
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-09-18 14:48:06 -0300

    conexao corrigida

--- models/connection.js
+++ models/connection.js
@@ -6,11 +6,8 @@ const OPTIONS = {
     useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
 .connect(MONGO_DB_URL, OPTIONS)
