e1c99b028214a9429e094c6616886336240f9a1b
Nathalia Zebral <natzebral@gmail.com>
2021-09-18 09:15:30 -0300

    cria pastas model, service e controller , conexoes e rotas

---
+++ models/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+// Para os testes locais funcionarem descomente as linhas acima.
+
+// Para o avaliador funcionar descomente as linhas abaixo:
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
+const connections = async () => MongoClient
+  .connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((connection) => connection.db(DB_NAME))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+module.exports = connections;
