eafaf92eeafcc0f8c561c66b23b685cc07e89bf8
Willian Alves <willian.alvesb65@gmail.com>
2021-09-18 20:02:24 -0300

    Fix: Ajustando URL de conectio

--- model/connection.js
+++ model/connection.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const OPTIONS = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 let db = null;
 
