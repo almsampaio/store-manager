0ad914bcb09350da4363340fd7e17dcffb85ce59
lucas-chave <lcchavesr150@gmail.com>
2021-09-19 20:28:22 -0300

    adicionando conexao com o banco de dados na camada modelo e algumas configuracoes  iniciais

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+const express = require('express');
+
+const app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-// iniciando o projeto
---
+++ models/conection.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+let db = null;
+
+const connection = () => (db ? Promise.resolve(db) : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+    db = conn.db(DB_NAME);
+    return db;
+  })
+);
+
+module.exports = connection;
