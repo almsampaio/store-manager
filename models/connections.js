c1c3488412de074684f3e96e4351beeddf239882
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-09-19 19:29:44 -0300

    Change URL

--- models/connections.js
+++ models/connections.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const DB_NAME = 'StoreManager';
-
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 // const DB_NAME = 'StoreManager';
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
   .connect(MONGO_DB_URL, {
