97d39e764c18921140abe1506b8e3db43509a7cd
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-19 15:55:13 -0300

    services tests - addSales and updateSales

--- test/unit/services.js
+++ test/unit/services.js
@@ -222,32 +222,138 @@ const salesModel = require('../../models/salesModel');
 
 describe('sales - testa o service addSales', () => {
   describe('ao tentar cadastrar uma venda inválida', () => {
-    it('quando a quantidade é inválida', async () => {});
-    it('quando o produto é inválido', async () => {});
-    it('quando não há estoque', async () => {});
+    it('quando a quantidade é inválida', async () => {
+      const addedSale = await salesService.addSales([{
+        productId: mockId,
+        quantity: -1,
+      }]);
+
+      expect(addedSale).to.deep.equal({
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity'
+      });
+    });
+    it('quando o produto é inválido', async () => {
+      const addedSale = await salesService.addSales([{
+        productId: '123456789',
+        quantity: 5,
+      }]);
+
+      expect(addedSale).to.deep.equal({
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity'
+      });
+    });
+    it('quando não há estoque', async () => {
+      sinon.stub(productsModel, 'getById').resolves({
+        _id: mockId,
+        name: 'produto',
+        quantity: 3
+      });
+
+      const addedSale = await salesService.addSales([{
+        productId: mockId,
+        quantity: 5,
+      }]);
+
+      expect(addedSale).to.deep.equal({
+        code: 'stock_problem',
+        message: 'Such amount is not permitted to sell'
+      });
+      productsModel.getById.restore();
+    });
   });
   describe('ao cadastrar uma venda válida', () => {
-    it('cadastro ocorre com sucesso', async () => {});
+    it('cadastro ocorre com sucesso', async () => {
+      sinon.stub(productsModel, 'getById').resolves({
+        _id: mockId,
+        name: 'produto',
+        quantity: 15
+      });
+      sinon.stub(productsModel, 'updateProductQty').resolves({
+        _id: mockId,
+        quantity: 10
+      });
+      sinon.stub(salesModel, 'addSales').resolves({
+        _id: mockId,
+        itensSold: [{ productId: mockId, quantity: 5 }]
+      });
+
+      const addedSale = await salesService.addSales([{
+        productId: mockId,
+        quantity: 5,
+      }]);
+
+      expect(addedSale).to.deep.equal({
+        _id: '604cb554311d68f491ba5781',
+        itensSold: [{ productId: '604cb554311d68f491ba5781', quantity: 5 }]
+      });
+
+      productsModel.getById.restore();
+      productsModel.updateProductQty.restore();
+      salesModel.addSales.restore();
+    });
   })
 });
 
 describe('sales - testa o service updateSales', () => {
   describe('ao tentar atualizar uma venda inválida', () => {
-    it('quando a quantidade é inválida', async () => {});
-    it('quando o produto é inválido', async () => {});
-    it('quando a venda não existe', async () => {});
-    it('quando não há estoque', async () => {});
+    it('quando a quantidade é inválida', async () => {
+      const updatedSale = await salesService.updateSales({
+        id: mockId,
+        productId: mockId,
+        quantity: -1,
+      });
+
+      expect(updatedSale).to.deep.equal({
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity'
+      });
+    });
+    it('quando o produto é inválido', async () => {
+      const updatedSale = await salesService.updateSales({
+        id: mockId,
+        productId: '123456789',
+        quantity: 10,
+      });
+
+      expect(updatedSale).to.deep.equal({
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity'
+      });
+    });
+    it('quando a venda não existe', async () => {
+      sinon.stub(salesModel, 'getById').resolves(null)
+      sinon.stub(productsModel, 'getById').resolves({
+
+      });
+
+      const updatedSale = await salesService.updateSales({
+        id: '12345789',
+        productId: mockId,
+        quantity: 10,
+      });
+
+      expect(updatedSale).to.deep.equal({
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity'
+      });
+
+      salesModel.getById.restore();
+      productsModel.getById.restore();
+    });
+    it('quando não há estoque', async () => { });
   });
   describe('ao atualizar uma venda válida', () => {
-    it('venda é atualizada', async () => {})
+    it('venda é atualizada', async () => { })
   });
 });
 
 describe('sales - testa o service deleteSales', () => {
   describe('ao tentar deletar uma venda inválida', () => {
-    it('venda não existe', async () => {});
+    it('venda não existe', async () => { });
   })
   describe('ao tentar deletar uma venda válida', () => {
-    it('venda é deletada com sucesso', async () => {});
+    it('venda é deletada com sucesso', async () => { });
   })
 });
