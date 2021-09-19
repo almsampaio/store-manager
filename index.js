7d03529b99b4448acb0d81bdf5aae3b8da96bfc7
IzeldaValeriano <zel.valeriano@gmail.com>
2021-09-19 10:45:22 -0300

    inicio de projeto

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+
+const PORT = 3000;
+const app = express();
+app.use(BodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
