f671ea0114d1ff128416b273702493e55dda7172
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-09-19 16:24:32 -0300

    Fix Requirement 1

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,13 @@
 const productService = require('../services/productService');
 
+const INVALID_DATA = 'invalid_data';
+const HTTP_CREATED = 201;
+
 const create = async (req, res) => {
   const { name, quantity } = req.body;
   const { status, data, message } = await productService.create(name, quantity);
 
-  const INVALID_DATA = 'invalid_data';
-  const HTTP_CREATED = 201;
-
-  if (message) {
-    return res.status(status).json(
-    { err: 
-      { 
-        code: INVALID_DATA, 
-        message, 
-    },
-    },
-      );
-    }
+  if (message) return res.status(status).json({ err: { code: INVALID_DATA, message } });
   res.status(HTTP_CREATED).json(data);
 };
 
--- middlewares/productMiddleware.js
+++ middlewares/productMiddleware.js
@@ -8,37 +8,43 @@ const QUANTITY_MUST_BE_NUMBER = '"quantity" must be a number';
 
 const validName = (req, res, next) => {
   const { name } = req.body;
-
   if (typeof name !== 'string') {
     return res.status(HTTP_UNPROCESSABLE_ENTITY).json({
-      code: INVALID_DATA,
-      message: NAME_STRING,
+      err: {
+        code: INVALID_DATA,
+        message: NAME_STRING,
+      },
     });
   }
 
   if (name.length <= 5) {
-    return res.status(HTTP_UNPROCESSABLE_ENTITY).json({
-      code: INVALID_DATA,
-      message: NAME_LENGTH,
+    return res.status(422).json({
+      err: {
+        code: INVALID_DATA,
+        message: NAME_LENGTH,
+      },
     });
   }
+
   next();
 };
 
 const validQuantity = (req, res, next) => {
   const { quantity } = req.body;
-
   if (typeof quantity !== 'number') {
-    return res.status(HTTP_UNPROCESSABLE_ENTITY).json({
-      code: INVALID_DATA,
-      message: QUANTITY_MUST_BE_NUMBER,
+    return res.status(422).json({
+      err: {
+        code: INVALID_DATA,
+        message: QUANTITY_MUST_BE_NUMBER,
+      },
     });
   }
-
   if (quantity <= 0) {
-    return res.status(HTTP_UNPROCESSABLE_ENTITY).json({
-      code: INVALID_DATA,
-      message: QUANTITY_LENGTH,
+    return res.status(422).json({
+      err: {
+        code: INVALID_DATA,
+        message: QUANTITY_LENGTH,
+      },
     });
   }
 
