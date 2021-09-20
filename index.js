9425613884e399671829edce8b6426b0215d4097
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-09-20 16:04:06 -0300

    inserido express rescue e error middleware

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
 const { products, sales } = require('./controllers/index');
+const { errorMiddleware } = require('./middlewares/errorMiddleware');
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
 
-app.post('/products', products.createProduct);
+app.post('/products', rescue(products.createProduct));
 app.post('/sales', sales.createSale);
+
+app.use(errorMiddleware);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
