c8b76dfd08361f076d2d89721fcf5fa1c01575a9
ScriptCamilo <script.camilo@gmail.com>
2021-09-20 20:15:34 -0300

    feat: getAll and getById services created

--- services/productsService.js
+++ services/productsService.js
@@ -9,6 +9,22 @@ async function create(name, quantity) {
   return newProductId;
 }
 
+async function getAll() {
+  const allDocuments = await productsModel.getAll();
+
+  return allDocuments;
+}
+
+async function getById(id) {
+  await validations.isIdValid(id);
+
+  const document = await productsModel.getById(id);
+
+  return document;
+}
+
 module.exports = {
   create,
+  getAll,
+  getById,
 };
--- services/validations/productsValidation.js
+++ services/validations/productsValidation.js
@@ -1,16 +1,16 @@
 const Validation = require('./index');
-const { findByName } = require('../../models/productsModel');
+const productsModel = require('../../models/productsModel');
 
 async function isNameValid(name) {
-  const nameNotExist = !await findByName(name); // if null(falsy) name is valid because doesn't exist yet
+  const isNameTrue = !await productsModel.getByName(name); // if null(falsy) name is valid because doesn't exist yet
 
   const mustBeString = new Validation(typeof name === 'string');
   const lengthFiveMore = new Validation(name.length >= 5);
-  const alreadyExists = new Validation(nameNotExist);
+  const nameExist = new Validation(isNameTrue);
 
   mustBeString.validate('"name" must be a string');
   lengthFiveMore.validate('"name" length must be at least 5 characters long');
-  alreadyExists.validate('Product already exists');
+  nameExist.validate('Product already exists');
 }
 
 function isQuantityValid(quantity) {
@@ -21,7 +21,16 @@ function isQuantityValid(quantity) {
   greaterThanZero.validate('"quantity" must be larger than or equal to 1');
 }
 
+async function isIdValid(id) {
+  const isIdTrue = !!await productsModel.getById(id);
+
+  const idExist = new Validation(isIdTrue);
+
+  idExist.validate('Wrong id format');
+}
+
 module.exports = {
   isNameValid,
   isQuantityValid,
+  isIdValid,
 };
