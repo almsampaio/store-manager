7999b59df700c3aded26a44b607426168b455269
lmuffato <lmiremuffato@gmail.com>
2021-09-20 20:05:30 -0300

    requisito 6

--- models/connectionMongodb.js
+++ models/connectionMongodb.js
@@ -21,7 +21,7 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager'; // Avaliador
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // Conexão normal
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // Conexão normal.
 
 const OPTIONS = {
   useNewUrlParser: true,
