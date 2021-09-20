b195d2b6989a4fa46d61a70a0b3b8daf541bb64c
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-09-20 14:43:37 -0300

    Adicionado docstring para a função de create

--- models/index.js
+++ models/index.js
@@ -1,3 +1,10 @@
+/**
+ * Simple create function to register data at the DB.
+ * @param {function} connection The connection DB obj.
+ * @param {string} collection String with the name of the Mongo Collection.
+ * @param {object} payload The object you want input on the DB.
+ * @returns The data inserted on the DB with the ID designated by the DB 
+ */
 const create = async (connection, collection, payload) => {
   const cnt = await connection();
   const { ops: [insertedData] } = await cnt.collection(collection).insertOne({
