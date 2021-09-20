94dbce3627ac7996e3ab3d5fa4762e8ec2f1e442
ScriptCamilo <script.camilo@gmail.com>
2021-09-20 20:14:43 -0300

    feat: get routers created

--- routes/products/index.js
+++ routes/products/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const productsController = require('../../controllers/productsController');
 
+// GET
+router.get('/', productsController.getAll);
+router.get('/:id', productsController.getById);
+
+// POST
 router.post('/', productsController.create);
 
 module.exports = router;
