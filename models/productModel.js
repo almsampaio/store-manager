6051b15abe462dae1bc01a0268456ad3f4367281
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-09-20 14:28:39 -0300

    corrige referencia de acordo com o manual do estudante

--- models/productModel.js
+++ models/productModel.js
@@ -5,7 +5,7 @@ const addProduct = async (name, quantity) => {
   const db = await connection();
   const result = await db.collection('products').insertOne({ name, quantity });
   return result.ops[0];
-  // retirado a informação do link: https://www.w3schools.com/nodejs/nodejs_mongodb_insert.asp
+  /** Source: https://www.w3schools.com/nodejs/nodejs_mongodb_insert.asp */
 };
 
 const getProductByName = async (name) => {
