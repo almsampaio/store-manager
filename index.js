6eefeb026220cc480397aead0f85286e6c036647
William <w.willers@outlook.com>
2021-09-20 21:22:32 -0400

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(bodyParser);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
