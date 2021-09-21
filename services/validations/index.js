09cbec82f9d5ffc02b3b2e728ea050f14e660301
ScriptCamilo <script.camilo@gmail.com>
2021-09-21 09:26:57 -0300

    refactor: validation functions and files

--- services/validations/index.js
+++ services/validations/index.js
@@ -1,19 +1,44 @@
-function Validation(condition) {
-  this.statusCode = { status: 422, code: 'invalid_data' };
-  this.condition = condition;
+const { ObjectId } = require('mongodb');
+const Validation = require('./constructor');
+
+async function isNameValid(name, verifyExistence) {
+  const mustBeString = new Validation(typeof name === 'string');
+  const lengthFiveMore = new Validation(name.length >= 5);
+
+  mustBeString.validate('"name" must be a string');
+  lengthFiveMore.validate('"name" length must be at least 5 characters long');
+
+  if (verifyExistence) {
+    const isExistenceFalse = !await verifyExistence(name); // if null(falsy) name is valid because doesn't exist yet
+    const nameExist = new Validation(isExistenceFalse);
+    nameExist.validate('Product already exists');
+  }
 }
 
-Validation.prototype.newError = function newError(status, code, message) {
-  const error = new Error(message);
-  error.status = status;
-  error.code = code;
-  throw error;
+function isQuantityValid(quantity, genericMessage) {
+  const mustBeNumber = new Validation(typeof quantity === 'number');
+  const mustBeNumberMessage = '"quantity" must be a number';
+
+  const greaterThanZero = new Validation(quantity >= 1);
+  const greaterThanZeroMessage = '"quantity" must be larger than or equal to 1';
+
+  mustBeNumber.validate(genericMessage || mustBeNumberMessage);
+  greaterThanZero.validate(genericMessage || greaterThanZeroMessage);
+}
+
+async function isIdValid(id, verifyExistence, genericMessage) {
+  const idErrorMsg = genericMessage || 'Wrong id format';
+
+  const isObjectIdValid = new Validation(ObjectId.isValid(id));
+  isObjectIdValid.validate(idErrorMsg);
+
+  const isExistenceTrue = !!await verifyExistence(id);
+  const idExist = new Validation(isExistenceTrue);
+  idExist.validate(idErrorMsg);
+}
+
+module.exports = {
+  isNameValid,
+  isQuantityValid,
+  isIdValid,
 };
-
-Validation.prototype.validate = function validate(message) {
-  const { status, code } = this.statusCode;
-
-  if (!this.condition) this.newError(status, code, message);
-};
-
-module.exports = Validation;
