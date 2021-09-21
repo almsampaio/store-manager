6737a04e0c821ab15a505b109ec383e5f48a1abf
lucas-chave <lcchavesr150@gmail.com>
2021-09-21 09:47:32 -0300

    implementaçao da funcao de verificacao da url de adicionar produto e correcoes do mesmo arquivo

--- service/Products.js
+++ service/Products.js
@@ -1,23 +1,42 @@
-// const { addProduct } = require('../models/Products');
+const { getAllProducts } = require('../models/Products');
+
+const INVALID_DATA = 'invalid_data';
+const STATUS_HTTP_422 = 422;
 
 const validateProduct = async (name, quantity) => {
   if (name.length < 5) {
     return (
-      { err: { status: 422, message: '"name" length must be at least 5 characters long' } }
+      { err: { code: INVALID_DATA, message: '"name" length must be at least 5 characters long' }, 
+      status: 422 }
      );
   }
   if (!quantity || quantity < 0) {
-    return (
-      { err: { status: 422, message: 'quantity must be larger than or equal to 1' } }
+    return ({ err: { code: INVALID_DATA, message: '"quantity" must be larger than or equal to 1' }, 
+      status: 422 }
     );
   }
   if (typeof quantity !== 'number') {
     return (
-      { err: { status: 422, message: '"quantity" must be a number' } }
+      { err: 
+        { code: INVALID_DATA, message: '"quantity" must be a number' },
+        status: STATUS_HTTP_422 }
     );
+  }
+};
+
+const validateQuantityProduct = async (name) => {
+  const products = await getAllProducts();
+  const verifyExistsName = products.some((product) => product.name === name);
+  
+  if (verifyExistsName) {
+    return ({ err: { 
+      code: INVALID_DATA, 
+      message: 'Product already exists' }, 
+      status: STATUS_HTTP_422 });
   }
 };
 
 module.exports = {
   validateProduct,
+  validateQuantityProduct,
 };
