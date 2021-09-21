2c3902e8f7b2192603315fd43f53618acc40c20c
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-09-21 12:38:09 -0300

    feat: agradecimentos, arquivo Agradecimentos na raiz

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-// const { Router } = require('express');
-
-// const router = new Router();
-
-// router.get('/', (_req, res) => {
-//   res.status(200).json({ Ok: 'oi' });
-// });
-
-// module.exports = router;
