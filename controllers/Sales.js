753adf81c3f82aae8990d7bd141b1c86ae7c2a24
Anderson Silva <andssilva.23@gmail.com>
2021-09-20 23:27:48 -0400

    feat(req 5): :rocket: create sales

--- controllers/Sales.js
+++ controllers/Sales.js
@@ -5,7 +5,8 @@ const HTTP_UNPROCESSABLE_STATUS = 422;
 const HTTP_OK_STATUS = 200;
 
 const createSales = async (request, response) => {
-  const { sale } = request.body;
+  const sale = request.body;
+  // console.log(sale);
   const validyValues = await create(sale);
   if (validyValues.err) return response.status(HTTP_UNPROCESSABLE_STATUS).json(validyValues);
   return response.status(HTTP_OK_STATUS).json(validyValues);
--- models/Sales.js
+++ models/Sales.js
@@ -4,7 +4,7 @@ const connection = require('./connection');
 const createSales = async (arrSales) => {
   const db = await connection();
   const products = await db.collection('sales').insertMany([{ arrSales }])
-    .then((result) => ({ _id: Object.values(result.insertedIds).toArray(), arrSales }));
+    .then((result) => ({ _id: Object.values(result.insertedIds).toString(), itensSold: arrSales }));
 
   return products;
 };
