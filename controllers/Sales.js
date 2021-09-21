8f35b854003f0f4a8eb5549f7d9a37e38479fbbf
Anderson Silva <andssilva.23@gmail.com>
2021-09-20 23:12:02 -0400

    feat: :rocket: create controller sales file

---
+++ controllers/Sales.js
@@ -0,0 +1,16 @@
+const { create } = require('../services/Sales');
+
+const HTTP_UNPROCESSABLE_STATUS = 422;
+// const HTTP_CREATED_STATUS = 201;
+const HTTP_OK_STATUS = 200;
+
+const createSales = async (request, response) => {
+  const { sale } = request.body;
+  const validyValues = await create(sale);
+  if (validyValues.err) return response.status(HTTP_UNPROCESSABLE_STATUS).json(validyValues);
+  return response.status(HTTP_OK_STATUS).json(validyValues);
+};
+
+module.exports = {
+  createSales,
+};
