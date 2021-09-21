5dac08e4f98a8521d561474366d75231aa495d0c
samuelmmjr <samuelmmjunior@gmail.com>
2021-09-21 17:11:34 -0300

    feat:correçao lint blank line

--- models/connection.js
+++ models/connection.js
@@ -10,7 +10,6 @@ const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 // const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
-
 let db = null;
 
 const connection = () => (db
