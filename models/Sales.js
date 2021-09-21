3183e948275ce668373c9a2a257251a68939f268
Anderson Silva <andssilva.23@gmail.com>
2021-09-20 22:32:44 -0400

    feat: :rocket: create model sales file

---
+++ models/Sales.js
@@ -0,0 +1,14 @@
+// const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const createSales = async (arrSales) => {
+  const db = await connection();
+  const products = await db.collection('sales').insertMany([{ arrSales }])
+    .then((result) => ({ _id: Object.values(result.insertedIds).toArray(), arrSales }));
+
+  return products;
+};
+
+module.exports = {
+  createSales,
+};
