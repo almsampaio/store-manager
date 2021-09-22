6afcb83ff055fbd42c7f221ca8dd04dce6d3ce79
jepaur5 <jepaur5@gmail.com>
2021-09-22 05:58:23 -0300

    arrumando o arrow6

--- models/connection.js
+++ models/connection.js
@@ -1,3 +1,6 @@
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
@@ -5,19 +8,15 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = 'StoreManager';
-
 let db = null;
 
 const connection = () => {
   return db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-      .then((conn) => {
-        db = conn.db(DB_NAME);
-        return db;
-      });  
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    });
 };
 
 module.exports = connection;
