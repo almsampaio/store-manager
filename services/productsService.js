33e1e3abc7ff519f3f0a01080774bafdc0e80c44
jepaur5 <jepaur5@gmail.com>
2021-09-22 18:24:36 -0300

    requisito1-services-products

--- services/productsService.js
+++ services/productsService.js
@@ -1,6 +1,6 @@
 const productsModel = require('../models/productsModel');
 
-const add = async (name, quantity) => await productsModel.add(name, quantity)
+const add = async (name, quantity) => productsModel.add(name, quantity)
   .then((data) => data);
 
 module.exports = {
