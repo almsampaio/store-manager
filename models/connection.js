34a8489e9365a10b42bf8c9d79b58944da0092d4
jepaur5 <jepaur5@gmail.com>
2021-09-22 04:54:59 -0300

    arrumando o arrow5

--- models/connection.js
+++ models/connection.js
@@ -5,10 +5,8 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL_LOCALHOST = 'mongodb://localhost:27017/StoreManager';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-
 
 let db = null;
 
