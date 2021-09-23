102cfaa1e983ff862c0c0e12c2805891d39c954b
lucas-chave <lcchavesr150@gmail.com>
2021-09-23 18:29:05 -0300

    corrigindo collection que recebe as vendas

--- models/Sales.js
+++ models/Sales.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const create = async (sale) => {
   const operation = await connection()
-    .then((db) => db.collection('products').insertOne({ itensSold: sale }))
+    .then((db) => db.collection('sales').insertOne({ itensSold: sale }))
       .then((result) => result.ops[0]);
 
   return operation;
