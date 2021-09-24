7fa59501eaa25266feafa188903700befbd6f1ee
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-09-23 23:09:42 -0300

    requirement 2 fix

--- controller/products.js
+++ controller/products.js
@@ -19,12 +19,12 @@ const controllerGetById = async (req, res) => {
     return res.status(status).json({ err: { code: 'invalid_data', message } });
   }
 
-  res.status(status).json({ products: info });
+  res.status(status).json(info);
 };
 
 const controllerGetAll = async (_req, res) => {
   const { status, info } = await services.servicesGetAll();
-  res.status(status).json({ products: info });
+  res.status(status).json(info);
 };
 
 module.exports = {
