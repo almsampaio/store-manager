5f55ffdda1679d14955cdbe487233b66bc89ab55
unknown <antonioschappo@gmail.com>
2021-09-25 15:36:09 -0300

    fix connection

--- models/connection.js
+++ models/connection.js
@@ -11,7 +11,7 @@ const OPTIONS = {
     useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const db = null;
