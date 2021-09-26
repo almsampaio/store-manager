993517127a352d0c4268f69b9248abfa512071db
jlfagundes <jlfagundes@gmail.com>
2021-09-26 11:12:03 -0300

    implantation structure for use body

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const bodyParser = require('body-parser');
+
 // não remova esse endpoint, e para o avaliador funcionar
 const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(3001, () => console.log('API in running!'));
+app.listen(3000, () => console.log('API in running!'));
