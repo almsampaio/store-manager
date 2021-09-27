d9e5e48ea6c37beaf26731b94d70a1f92429c340
Erick-Marinho <erickgaves@gmail.com>
2021-09-27 15:56:07 -0300

    Importando lib dotEnv, configurando e inserindo método listen

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const dotEnv = require('dotenv');
 
 const app = express();
+dotEnv.config();
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
