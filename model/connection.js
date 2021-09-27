4da3dd1e7f8df23a2ea94f050d864974447825e2
Erick-Marinho <erickgaves@gmail.com>
2021-09-27 17:02:25 -0300

    Criando conexão com banco de dados - mongodb

---
+++ model/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(process.env.MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+    db = conn.db(process.env.DB_NAME);
+    console.log('Database Connected');
+    return db;
+  })
+  .catch((error) => {
+    console.log('Database connectivity Error ===', error);
+    throw new Error(error);
+  })
+);
+
+module.exports = connection;
