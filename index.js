4d3e4f0ed471fae47eaf608652cb07029ce0b684
Erick-Marinho <erickgaves@gmail.com>
2021-09-27 13:34:53 -0300

    Importando e instânciando express

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
