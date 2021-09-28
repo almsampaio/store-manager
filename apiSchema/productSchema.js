e183a1c20d15641c69d50a409be8b97a44e10d86
Erick-Marinho <erickgaves@gmail.com>
2021-09-28 19:48:12 -0300

    Modificando método de joi para número mínimo

--- apiSchema/productSchema.js
+++ apiSchema/productSchema.js
@@ -3,6 +3,6 @@ const Joi = require('joi');
 module.exports.createProductSchema = Joi.object().keys({
   name: Joi.string().min(5).not().empty()
   .required(),
-  quantity: Joi.number().greater(0).not().empty()
+  quantity: Joi.number().min(1).not().empty()
   .required(),
 });
