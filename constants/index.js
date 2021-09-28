39486b95445df0230dd076dfc118871470cbeb78
Erick-Marinho <erickgaves@gmail.com>
2021-09-28 13:23:15 -0300

    Criando constants para menssagens

---
+++ constants/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  defaultServerResponse: {
+    status: 400,
+    message: '',
+    body: {},
+  },
+  productMessage: {
+    PRODUCT_CREATED: 'Product Created Sucessfully',
+    DUPLICATE_NAME: 'Name already exist with given name',
+  },
+  requestValidationMessage: {
+    BAD_REQUEST: 'Invalid fields',
+  },
+};
