328de713b64da1db46590552af952f2b326efc5d
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-09-28 20:35:01 -0300

    fix:  modifica código de conexão com o banco de dados

--- models/conection.js
+++ models/conection.js
@@ -1,20 +1,25 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+const OPTIONS = {
+  urlNewParser: true,
+  useUniFieldTopology: true,
+};
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
+let db = null;
+
 const connection = () => (
-  MongoClient.connect(MONGO_DB_URL, {
-    urlNewParser: true,
-    useUniFieldTopology: true,
-  })
-  .then((conn) => conn.db(DB_NAME))
-  .catch((err) => {
-    console.error(err);
-    process.exit();
+  db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+    db = conn.db(DB_NAME);
+    return db;
   })
 );
 
