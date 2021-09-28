7c999efa89fcccfa45c346e43b8b8bc1d6f37308
Erick-Marinho <erickgaves@gmail.com>
2021-09-28 19:48:55 -0300

    Incluindo respostas da solicitação

--- controller/productController.js
+++ controller/productController.js
@@ -2,15 +2,22 @@ const productService = require('../service/productService');
 const constants = require('../constants');
 
 module.exports.createProduct = async (req, res) => {
+  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
+    condition = true;
     const responseFromService = await productService.createProduct(req.body);
-    response.status = 200;
-    response.message = constants.productMessage.PRODUCT_CREATED;
+    response.status = 201;
     response.body = responseFromService;
+    delete response.err;
   } catch (error) {
+    condition = false;
     console.log('Something went wrong: Controller: createProduct', error);
-    response.message = error.message;
+    response.status = 422;
+    response.message = constants.productMessage.DUPLICATE_NAME;
+    response.err.code = constants.productMessage.DATA_INVALID;
+    response.err.message = error.message.slice(7);
+    delete response.body;
   }
-  return res.status(response.status).send(response);
+  return res.status(response.status).send(condition ? response.body : response);
 };
