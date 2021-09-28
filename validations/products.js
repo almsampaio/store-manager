f9a1990f5bcf83ecf6b0d48b00200fd3c1b91e66
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-28 15:03:12 -0400

    feat: products inserted data validations

--- validations/products.js
+++ validations/products.js
@@ -1,9 +1,41 @@
-const validateInsertData = (req, res, next) => {
-    const { name, quantity } = req.body;
+const service = require('../service/productsService');
+
+const validateInsertedName = async (req, res, next) => {
+    const { name } = req.body;
+    const allProducts = await service.getAllProducts();
     if (name.length < 5) {
+      res.status(422).json({ err: { 
+          code: 'invalid_data',
+          message: '\'name\' length must be at least 5 characters long',
+       } });
+    }
+    if (allProducts.some((product) => product.name === name)) {
+      res.status(422).json({ err: { 
+          code: 'invalid_data',
+          message: 'Product already exists',
+      } });
+    }
+    next();
+};
+
+const validateInsertedQtd = (req, res, next) => {
+  const { quantity } = req.body;
+  if (quantity <= 0) {
     res.status(422).json({ err: { 
         code: 'invalid_data',
-        message: '\'name\' length must be at least 5 characters long',
-     } });
-    }
+        message: '\'quantity\' must be larger than or equal to 1',
+    } });
+  }
+  if (typeof quantity !== 'number') {
+    res.status(422).json({ err: { 
+        code: 'invalid_data',
+        message: '\'quantity\' must be a number',
+    } });
+  }
+  next();
+};
+
+module.exports = {
+    validateInsertedName,
+    validateInsertedQtd,
 };
