a44bf4e072b0c9607a8b9dec7b523ef9791cca20
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-28 14:50:30 -0400

    fix: connection params

--- model/connection.js
+++ model/connection.js
@@ -1,16 +1,19 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_URL = 'mongodb://mongodb:27017/StoreManager';
+const OUTPUT = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017';
 const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
-    .connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }).then((conn) => conn.db(DB_NAME))
-    .catch((err) => {
-      console.error(err);
-      process.exit();
-    });
+  .connect(MONGO_DB_URL, OUTPUT)
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 module.exports = connection;
