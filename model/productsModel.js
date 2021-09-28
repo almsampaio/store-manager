ea93cd90bf039954519ea64987d964a7fd9df40b
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-28 14:51:19 -0400

    feat: model that returns all products

--- model/productsModel.js
+++ model/productsModel.js
@@ -2,16 +2,26 @@ const connection = require('./connection');
 
 const collection = 'products';
 
+const getAllProducts = async () => {
+  try {
+    const result = await connection().then((db) => db.collection(collection).find().toArray());
+    return result;
+  } catch (error) {
+    return error.message;
+  }
+};
+
 const addNewProduct = async (newProduct) => {
   try {
-    await connection().then((db) => db.collection(collection))
-    .insertOne(newProduct);
-    return 'Produto adicionado com sucesso!';
+    const result = await connection().then((db) => db.collection(collection)
+      .insertOne(newProduct));
+    return result.ops.pop();
   } catch (error) {
-    return error;
+    return error.message;
   }
 };
 
 module.exports = {
     addNewProduct,
+    getAllProducts,
 };
