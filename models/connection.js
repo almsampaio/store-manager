85b486ef7ade82515469763e84d25cfeb52e752b
jlfagundes <jlfagundes@gmail.com>
2021-09-28 08:58:08 -0300

    feat: connection DB create

---
+++ models/connection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const DBNAME = 'StoreManager';
+
+let db = null;
+
+const connectionDb = () => (db ? Promise.resolve(db) : MongoClient.connect(MONGO_DB_URL, OPTIONS))
+  .then((con) => {
+    db = con.db(DBNAME);
+    return db;
+  });
+
+  module.exports = {
+    connectionDb,
+  };
