21100ec067f3d3256f055abfdbd2f6ed080bcb5f
Erick-Marinho <erickgaves@gmail.com>
2021-09-28 19:47:33 -0300

    Extraindo informação de erro e criando resposta da solicitação

--- middlewares/joiSchemaValidation.js
+++ middlewares/joiSchemaValidation.js
@@ -4,9 +4,9 @@ const constants = require('../constants');
 const validateObjectSchema = (data, schema) => {
   const result = schema.validate(data, { convert: false });
   if (result.error) {
-    const errorDetails = result.error.details.map((value) => (
-      { error: value.message, path: value.path }));
-    return errorDetails;
+    const errorDetails = result.error.details.map((value) => (value.message));
+    const [errorMessage] = errorDetails; 
+    return errorMessage;
   }
   return null;
 };
@@ -15,8 +15,11 @@ module.exports.validateBody = (schema) => ((req, res, next) => {
   const response = { ...constants.defaultServerResponse };
   const error = validateObjectSchema(req.body, schema);
   if (error) {
-    response.body = error;
+    response.status = 422;
     response.message = constants.requestValidationMessage.BAD_REQUEST;
+    response.err.code = constants.productMessage.DATA_INVALID;
+    response.err.message = error;
+    delete response.body;
     return res.status(response.status).send(response);
   }
   return next();
