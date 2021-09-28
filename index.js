c0780014c61984dbea9d9b8738f5ba1685c319b1
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-27 23:56:18 -0400

    feat: model > connection to mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+const PORT = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.send();
+  response.send('Hello World 🧑‍🚀🚀');
 });
+
+app.listen(PORT, () => console.log(`WE'RE RUNING ON ${PORT}`))
