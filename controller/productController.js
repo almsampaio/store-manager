6c2575f668626ec5717c021f8e57a2d4d1986f9e
Erick-Marinho <erickgaves@gmail.com>
2021-09-29 13:26:38 -0300

    Criando controller para obter todos os produtos

--- controller/productController.js
+++ controller/productController.js
@@ -21,3 +21,24 @@ module.exports.createProduct = async (req, res) => {
   }
   return res.status(response.status).send(condition ? response.body : response);
 };
+
+module.exports.getAllProducts = async (req, res) => {
+  let condition;
+  const response = { ...constants.defaultServerResponse };
+  try {
+    condition = true;
+    const responseFromService = await productService.getAllProducts();
+    response.status = 200;
+    response.body = responseFromService;
+    delete response.err;
+  } catch (error) {
+    condition = false;
+    console.log('Something went wrong: Controller: getAllProducts', error);
+    response.status = 422;
+    response.message = constants.productMessage.PRODUCT_FETCHED;
+    response.err.code = constants.productMessage.DATA_INVALID;
+    response.err.message = error.message.slice(7);
+    delete response.body;
+  }
+  return res.status(response.status).send(condition ? response.body : response);
+};
