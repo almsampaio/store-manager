9b357af0b4f5988d84efe7987fc4547c68f9e72c
Erick-Marinho <erickgaves@gmail.com>
2021-09-29 13:26:20 -0300

    Criando constante com informação de produtos obtidos

--- constants/index.js
+++ constants/index.js
@@ -9,10 +9,11 @@ module.exports = {
     body: {},
   },
   productMessage: {
-    PRODUCT_CREATED: 'Product Created Sucessfully',
+    PRODUCT_CREATED: 'Product Created Successfully',
     DUPLICATE_NAME: 'Name already exist with given name',
     DATA_INVALID: 'invalid_data',
     PRODUCT_EXISTING: 'Product already exists',
+    PRODUCT_FETCHED: 'Product Fetched Successfuly',
   },
   requestValidationMessage: {
     BAD_REQUEST: 'Invalid fields',
