17eff673cb95b6d74f981bbc4e70ad24159bd692
Erick-Marinho <erickgaves@gmail.com>
2021-09-29 13:27:11 -0300

    Definindo router para obtenção e filtragem de produtos

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,4 +9,6 @@ router.post('/',
   joiSchemaValidation.validateBody(productSchema.createProductSchema), 
   productController.createProduct);
 
+router.get('/', productController.getAllProducts);
+
 module.exports = router;
