569dbe2d50e3b7b41c3d4e61993f4b1f169180f2
Erick-Marinho <erickgaves@gmail.com>
2021-09-29 19:55:35 -0300

    Criando controller para deletar produtos

--- controller/productController.js
+++ controller/productController.js
@@ -78,3 +78,22 @@ module.exports.updateProduct = async (req, res) => {
   }
   return res.status(response.status).send(condition ? response.body : response);
 };
+
+module.exports.deleteProduct = async (req, res) => {
+  let condition;
+  const response = { ...constants.defaultServerResponse };
+  try {
+    condition = true;
+    const responseFromService = await productService.deleteProduct(req.params);
+    response.status = 200;
+    response.body = responseFromService;
+    delete response.err;
+  } catch (error) {
+    condition = false;
+    response.status = 422;
+    response.err.code = constants.productMessage.DATA_INVALID;
+    response.err.message = error.message.slice(7);
+    delete response.body;
+  }
+  return res.status(response.status).send(condition ? response.body : response);
+};
