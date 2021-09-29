3c46198bebf57ecd3191c198896ec2ab95fcc986
Erick-Marinho <erickgaves@gmail.com>
2021-09-29 18:41:37 -0300

    Removendo campo não útil e reordenando constants

--- constants/index.js
+++ constants/index.js
@@ -1,7 +1,6 @@
 module.exports = {
   defaultServerResponse: {
     status: '',
-    message: '',
     err: {
       code: '',
       message: '',
@@ -9,12 +8,13 @@ module.exports = {
     body: {},
   },
   productMessage: {
+    PRODUCT_FETCHED: 'Product Fetched Successfuly',
+    PRODUCT_UPDATED: 'Product Updated Successfully',
     PRODUCT_CREATED: 'Product Created Successfully',
+    PRODUCT_EXISTING: 'Product already exists',
+    PRODUCT_NOT_FOUND: 'Product Not Found',
     DUPLICATE_NAME: 'Name already exist with given name',
     DATA_INVALID: 'invalid_data',
-    PRODUCT_EXISTING: 'Product already exists',
-    PRODUCT_FETCHED: 'Product Fetched Successfuly',
-    PRODUCT_NOT_FOUND: 'Product Not Found',
   },
   requestValidationMessage: {
     BAD_REQUEST: 'Invalid fields',
