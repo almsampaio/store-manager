53bba29ff8a578d35a3c7cd659c67ccc659ec5c3
jlfagundes <jlfagundes@gmail.com>
2021-09-30 08:05:51 -0300

    feat: challenge 5 crate endpoint new sale

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 
 const express = require('express');
 const controllerProducts = require('./controllers/products');
+const controllerSales = require('./controllers/sales');
 
 // não remova esse endpoint, e para o avaliador funcionar
 
@@ -18,5 +19,7 @@ app.get('/products', controllerProducts.getAll);
 app.get('/products/:id', controllerProducts.searchById);
 app.put('/products/:id', controllerProducts.updateProduct);
 app.delete('/products/:id', controllerProducts.deleteProduct);
+
+app.post('/sales', controllerSales.inputSales);
 
 app.listen(3000, () => console.log('API in running!'));
