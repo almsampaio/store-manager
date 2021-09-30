2861d0d08628d4e76f9d924e03148336d2253752
Erick-Marinho <erickgaves@gmail.com>
2021-09-30 20:40:12 -0300

    Criando service para vendas

--- service/salesService.js
+++ service/salesService.js
@@ -1,5 +1,7 @@
-// const connection = require('../model/connection');
+const { ObjectId } = require('mongodb');
+const connection = require('../model/connection');
 const salesModel = require('../model/salesModel');
+const constants = require('../constants');
 
 module.exports.createSales = async (serviceData) => {
   try {
@@ -7,6 +9,34 @@ module.exports.createSales = async (serviceData) => {
     return sales;
   } catch (error) {
     console.log('Something went wrong: Service createSales', error);
+    throw new Error(error);
+  }
+};
+
+module.exports.getAllSales = async () => {
+  try {
+    const sales = await connection()
+        .then((db) => db.collection('sales').find().toArray());
+      return { sales };
+  } catch (error) {
+    console.log('Something went wrong: Service getAllSales', error);
+    throw new Error(error);
+  }
+};
+
+module.exports.getsalesById = async ({ id }) => {
+  try {
+    if (!ObjectId.isValid(id)) {
+      throw new Error(constants.dataBaseMessage.INVALID_ID);
+    }
+    const product = await connection()
+      .then((db) => db.collection('sales').findOne({ _id: ObjectId(id) }));
+    if (!product) {
+      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+    }
+    return product;
+  } catch (error) {
+    console.log('Something went wrong: Service getsalesById', error);
     throw new Error(error);
   }
 };
