a194554d77cddfb4609925bec50f3b2d81834c5a
Erick-Marinho <erickgaves@gmail.com>
2021-09-30 20:39:49 -0300

    Criando controller para vendas

--- controller/salesController.js
+++ controller/salesController.js
@@ -13,3 +13,29 @@ module.exports.createSales = async (req, res) => {
   }
   return res.status(response.status).send(response.body);
 };
+
+module.exports.getAllSales = async (_req, res) => {
+  const response = { ...constants.salesMessage };
+  try {
+    const responseFromService = await salesService.getAllSales();
+    response.status = 200;
+    response.body = responseFromService;
+  } catch (error) {
+    response.status = 422;
+    delete response.body;
+  }
+  return res.status(response.status).send(response.body);
+};
+
+module.exports.getsalesById = async (req, res) => {
+  const response = { ...constants.salesMessage };
+  try {
+    const responseFromService = await salesService.getsalesById(req.params);
+    response.status = 200;
+    response.body = responseFromService;
+  } catch (error) {
+    response.status = 404;
+    response.body = { err: { code: 'not_found', message: 'Sale not found' } };
+  }
+  return res.status(response.status).send(response.body);
+};
