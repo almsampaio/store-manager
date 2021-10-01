be64e66ab5451b0f0c5a62f3e15c332a65e7429b
Erick-Marinho <erickgaves@gmail.com>
2021-09-30 21:44:06 -0300

    Criando router para exclusão de vendas

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -6,13 +6,15 @@ const salesSchema = require('../apiSchema/salesSchema');
 const router = express.Router();
 
 router.post('/', joiSchemaValidation.validateBody(salesSchema.createSalesSchema),
-salesController.createSales);
+  salesController.createSales);
 
-router.get('/:id', salesController.getsalesById);
+router.get('/:id', salesController.getSalesById);
 
 router.put('/:id', joiSchemaValidation.validateBody(salesSchema.updateSalesSchema),
   salesController.updateSales);
 
 router.get('/', salesController.getAllSales);
+
+router.delete('/:id', salesController.deleteSales);
 
 module.exports = router;
