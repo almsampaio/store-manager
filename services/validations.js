ab38f786eba35f67d35a76d0a4a6451e05f18cbd
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-10-02 18:43:37 -0300

    Requisitos 1 e 2 novamente

--- services/validations.js
+++ services/validations.js
@@ -5,79 +5,64 @@ const validateName = (name) => {
   const nameLength = 5;
 
   if (name.length < nameLength) {
-    try {
       return {
         status: HTTP_UNPROCESSABLE_ENTITY,
       };
-    } catch (error) {
-      throw new Error({
+    }
+    return {
         err: 'invalid_data',
         message: '"name" length must be at least 5 characters long',
-      });
-    }
-  } 
+    };
 };
 
 const alreadyExists = async (name) => {
   const result = await findByName(name);
 
   if (result) {
-    try {
       return {
         status: HTTP_UNPROCESSABLE_ENTITY,
-      };
-    } catch (error) {
-      throw new Error({
-        err: 'invalid_data',
-        message: 'Product already exists',
-      });
-    }
+    }; 
   }
-};
+  return {
+    err: 'invalid_data',
+    message: 'Product already exists',
+  };
+  };
 
 const validateQuantity = (quatity) => {
   if (quatity < 1) {
-    try {
       return {
         status: HTTP_UNPROCESSABLE_ENTITY,
       };
-    } catch (error) {
-      throw new Error({
-        err: 'invalid_data',
-        message: '"quantity" must be larger than or equal to 1',
-      });
     }
-  }
+    return {
+      err: 'invalid_data',
+      message: '"quantity" must be larger than or equal to 1',
+    };
 };
 
 const validateNumber = (quantity) => {
   if (typeof quantity !== 'number') {
-    try {
       return {
         status: HTTP_UNPROCESSABLE_ENTITY,
       };
-    } catch (error) {
-      throw new Error({
-        err: 'invalid_data',
-        message: '"quantity" must be a number',
-      });
     }
-  }
+    return {
+      err: 'invalid_data',
+      message: '"quantity" must be a number',
+    };
 };
 
 const validateSearch = (result) => {
   if (!result) {
-    try {
       return {
         status: HTTP_UNPROCESSABLE_ENTITY,
       };
-    } catch (error) {
-      throw new Error({
-        err: 'invalid_data',
-        message: 'Wrong id format',
-      });
     }
-  }
+    return {
+      err: 'invalid_data',
+      message: 'Wrong id format',
+    };
 };
 
 module.exports = {
