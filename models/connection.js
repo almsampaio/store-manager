40ee955e730c53139d1b575c56f1ebddbb1025ca
Renan Costa <renancosta21@gmail.com>
2021-10-03 15:19:50 -0300

    feat: creating connection

---
+++ models/connection.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require('mongodb');
+
+// linhas para o Avaliador
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
+// const DB_NAME = 'StoreManager';
+
+let db = null;
+
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }));
+
+module.exports = { connection };
