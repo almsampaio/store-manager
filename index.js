2ff2eb6218b7a30aadf07f6dbe0dd09c3d6ce16e
Renan Costa <renancosta21@gmail.com>
2021-10-03 15:28:41 -0300

    feat: config index.js + routes

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+const productController = require('./controllers/Products');
+
+const PORT = 3000;
+const app = express();
+
+app.use(BodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
+
+// routes para os Products
+app.get('/products', productController.getAll);
+app.post('/products', productController.create);
