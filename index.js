21989048e5d988c405fc608e2ade5a0f0e968187
Andre Sampaio <andresampaiolm@gmail.com>
2021-10-03 08:13:45 -0300

    HTTP_OK_STATUS never used fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ProductController = require('./controllers/ProductController');
 
-const HTTP_OK_STATUS = 200;
+// const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 const app = express();
