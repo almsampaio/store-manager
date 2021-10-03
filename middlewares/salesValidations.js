e59862fc0cbbbd7e2c5d4d147ed45465df4eb884
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-10-03 10:59:47 -0300

    requisito 10

--- middlewares/salesValidations.js
+++ middlewares/salesValidations.js
@@ -1,14 +1,21 @@
 const { ObjectId } = require('mongodb');
+const Products = require('../models/Products');
 
 const INVALID_DATA_ERROR_MESSAGE = 'Wrong product ID or invalid quantity';
 const INVALID_ID_FORMAT_ERROR_MESSAGE = 'Wrong sale ID format';
+const NOT_ALLOWED_AMOUNT_ERROR_MESSAGE = 'Such amount is not permitted to sell';
 
 function quantityValidation(req, _res, next) {
   const sales = [...req.body];
 
-  sales.forEach((sale) => {
+  sales.forEach(async (sale) => {
     if (sale.quantity <= 0 || typeof sale.quantity !== 'number') {
       next({ status: 422, code: 'invalid_data', message: INVALID_DATA_ERROR_MESSAGE });
+    }
+
+    const product = await Products.getProductById(ObjectId(sale.productId));
+    if (sale.quantity > product.quantity) {
+      next({ status: 404, code: 'stock_problem', message: NOT_ALLOWED_AMOUNT_ERROR_MESSAGE });
     }
   });
 
