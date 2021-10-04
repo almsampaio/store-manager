c3b1e007a46430d166c7366d6ffb98b0ee8bbe2f
Erick-Marinho <erickgaves@gmail.com>
2021-10-04 19:41:43 -0300

    Refatoração de código

--- service/salesService.js
+++ service/salesService.js
@@ -2,82 +2,95 @@ const { ObjectId } = require('mongodb');
 const connection = require('../model/connection');
 const salesModel = require('../model/salesModel');
 const constants = require('../constants');
-const { updateProductAfterSalesCreation, updateProductAfterDeletingSales } = require('../helpers');
+const { updateCreate, updateDelete } = require('../helpers');
+const { getProductById, updateProduct } = require('./productService');
 
-module.exports.createSales = async (serviceData) => {
-  try {
-    const sales = await salesModel.createSales(serviceData);
-    await updateProductAfterSalesCreation(sales);
-    return sales;
-  } catch (error) {
-    console.log('Something went wrong: Service createSales', error);
-    throw new Error(error);
-  }
+const newProduct = async ({ productId: id, quantity: saleQuantity }) => {
+  const { name, quantity: productQuantity } = await getProductById({ id });
+  const resultQuantity = productQuantity - saleQuantity;
+  const product = {
+    id,
+    updateInfo: {
+      name,
+      quantity: resultQuantity,
+    },
+  };
+  await updateProduct(product);
 };
 
-module.exports.getAllSales = async () => {
-  try {
-    const sales = await connection()
-        .then((db) => db.collection('sales').find().toArray());
-      return { sales };
-  } catch (error) {
-    console.log('Something went wrong: Service getAllSales', error);
-    throw new Error(error);
+const createSales = async (serviceData) => {
+  const resultStock = await Promise.all(serviceData.map(async (item) => updateCreate(item)));
+  const validate = resultStock.every((item) => item >= 0);
+  if (!validate) {
+    return {
+      status: 404,
+      message: {
+        err: { code: 'stock_problem', message: 'Such amount is not permitted to sell' },
+      },
+    };
   }
+  await Promise.all(serviceData.map(async (item) => newProduct(item)));
+  const sales = await salesModel.createSales(serviceData);
+  return { message: sales, status: 200 };
 };
 
-module.exports.getSalesById = async ({ id }) => {
-  try {
-    if (!ObjectId.isValid(id)) {
-      throw new Error(constants.dataBaseMessage.INVALID_ID);
-    }
-    const product = await connection()
-      .then((db) => db.collection('sales').findOne({ _id: ObjectId(id) }));
-    if (!product) {
-      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
-    }
-    return product;
-  } catch (error) {
-    console.log('Something went wrong: Service getSalesById', error);
-    throw new Error(error);
-  }
+const getAllSales = async () => {
+  const sales = await connection()
+    .then((db) => db.collection('sales').find().toArray());
+  return { sales };
 };
 
-module.exports.updateSales = async ({ id, updateInfo }) => {
+const getSalesById = async ({ id }) => {
+  if (!ObjectId.isValid(id)) {
+    return false;
+  }
+  const product = await connection()
+    .then((db) => db.collection('sales').findOne({ _id: ObjectId(id) }));
+  if (!product) {
+    return false;
+  }
+  return product;
+};
+
+const updateSales = async ({ id, updateInfo }) => {
   const [{ productId, quantity }] = updateInfo;
-  try {
+  
     if (!ObjectId.isValid(id)) {
-      throw new Error(constants.dataBaseMessage.INVALID_ID);
+      throw new Error(constants.FORMAT_ID);
     }
     const sales = await connection()
       .then((db) => db.collection('sales').updateOne(
         { _id: ObjectId(id) }, { $set: { itensSold: { productId, quantity } } },
       ));
     if (!sales) {
-      throw new Error(constants.productMessage.PRODUCT_NOT_FOUND);
+      throw new Error(constants.PRODUCT_NOT_FOUND);
     }
     const newSales = { _id: id, itensSold: [...updateInfo] };
     return newSales;
-  } catch (error) {
-    console.log('Something went wrong: Service updateSales', error);
-    throw new Error(error);
-  }
 };
 
-module.exports.deleteSales = async ({ id }) => {
-  try {
-    if (!ObjectId.isValid(id)) {
-      throw new Error(constants.dataBaseMessage.INVALID_ID);
-    }
-    const sale = await connection()
-      .then((db) => db.collection('sales').findOneAndDelete({ _id: ObjectId(id) }));
-    if (!sale) {
-      throw new Error({ error: constants.sales.SALES_NOT_FOUND });
-    }
-    await updateProductAfterDeletingSales(sale.value);
-    return sale.value;
-  } catch (error) {
-    console.log('Something went wrong: Service deleteSales', error);
-    throw new Error(error);
+const deleteSales = async ({ id }) => {
+  const sale = await getSalesById({ id }); 
+  console.log('sale service', sale);
+  if (!ObjectId.isValid(id) || !sale) {
+    return {
+    status: 422,
+    message: {
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong sale ID format',
+      },
+    } };
   }
+  await sale.itensSold.forEach((item) => updateDelete(item));
+  await salesModel.remove(id);
+  return { status: 200, message: sale };
+};
+
+module.exports = {
+  getSalesById,
+  deleteSales,
+  updateSales,
+  getAllSales,
+  createSales,
 };
