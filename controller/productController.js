0c7eb3a6271c980decb37e744334f27611d7121b
Erick-Marinho <erickgaves@gmail.com>
2021-10-04 19:41:25 -0300

    Refatoração de código

--- controller/productController.js
+++ controller/productController.js
@@ -2,98 +2,76 @@ const productService = require('../service/productService');
 const constants = require('../constants');
 
 module.exports.createProduct = async (req, res) => {
-  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
-    condition = true;
     const responseFromService = await productService.createProduct(req.body);
     response.status = 201;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
-    condition = false;
     response.status = 422;
-    response.err.code = constants.productMessage.DATA_INVALID;
-    response.err.message = error.message.slice(7);
-    delete response.body;
+    response.err.code = constants.DATA_INVALID;
+    response.err.message = error.message;
+    return res.status(response.status).send(response);
   }
-  return res.status(response.status).send(condition ? response.body : response);
 };
 
 module.exports.getAllProducts = async (req, res) => {
-  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
-    condition = true;
     const responseFromService = await productService.getAllProducts();
     response.status = 200;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
-    condition = false;
     response.status = 422;
-    response.err.code = constants.productMessage.DATA_INVALID;
-    response.err.message = error.message.slice(7);
-    delete response.body;
+    return res.status(response.status).send(error.message);
   }
-  return res.status(response.status).send(condition ? response.body : response);
 };
 
 module.exports.getProductById = async (req, res) => {
-  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
-    condition = true;
     const responseFromService = await productService.getProductById(req.params);
     response.status = 200;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
-    condition = false;
     response.status = 422;
-    response.err.code = constants.productMessage.DATA_INVALID;
-    response.err.message = error.message.slice(7);
-    delete response.body;
+    response.err.code = constants.DATA_INVALID;
+    response.err.message = error.message;
+    return res.status(response.status).send(response);
   }
-  return res.status(response.status).send(condition ? response.body : response);
 };
 
 module.exports.updateProduct = async (req, res) => {
-  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
-    condition = true;
     const responseFromService = await productService.updateProduct(
       { id: req.params.id, updateInfo: req.body },
     );
     response.status = 200;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
-    condition = false;
     response.status = 422;
-    response.err.code = constants.productMessage.DATA_INVALID;
-    response.err.message = error.message.slice(7);
-    delete response.body;
+    response.err.code = constants.DATA_INVALID;
+    response.err.message = error.message;
+    return res.status(response.status).send(response);
   }
-  return res.status(response.status).send(condition ? response.body : response);
 };
 
 module.exports.deleteProduct = async (req, res) => {
-  let condition;
   const response = { ...constants.defaultServerResponse };
   try {
-    condition = true;
     const responseFromService = await productService.deleteProduct(req.params);
     response.status = 200;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
-    condition = false;
     response.status = 422;
-    response.err.code = constants.productMessage.DATA_INVALID;
-    response.err.message = error.message.slice(7);
-    delete response.body;
+    response.err.code = constants.DATA_INVALID;
+    response.err.message = error.message;
+    return res.status(response.status).send(response);
   }
-  return res.status(response.status).send(condition ? response.body : response);
 };
--- controller/salesController.js
+++ controller/salesController.js
@@ -1,70 +1,61 @@
 const salesService = require('../service/salesService');
 const constants = require('../constants');
 
-module.exports.createSales = async (req, res) => {
-  const response = { ...constants.salesMessage };
-  try {
-    const responseFromService = await salesService.createSales(req.body);
-    response.status = 200;
-    response.body = responseFromService;
-  } catch (error) {
-    response.status = 422;
-    delete response.body;
-  }
-  return res.status(response.status).send(response.body);
+const createSales = async (req, res) => {
+  const { status, message } = await salesService.createSales(req.body);
+  return res.status(status).send(message);
 };
 
-module.exports.getAllSales = async (_req, res) => {
-  const response = { ...constants.salesMessage };
+const getAllSales = async (_req, res) => {
+  const response = { ...constants.defaultServerResponse };
   try {
     const responseFromService = await salesService.getAllSales();
     response.status = 200;
     response.body = responseFromService;
+    return res.status(response.status).send(response.body);
   } catch (error) {
     response.status = 422;
-    delete response.body;
+    return res.status(response.status).send(error.message);
   }
-  return res.status(response.status).send(response.body);
 };
 
-module.exports.getSalesById = async (req, res) => {
-  const response = { ...constants.salesMessage };
-  try {
+const getSalesById = async (req, res) => {
+  const response = { ...constants.defaultServerResponse };
     const responseFromService = await salesService.getSalesById(req.params);
     response.status = 200;
     response.body = responseFromService;
-  } catch (error) {
-    response.status = 404;
-    response.body = { err: { code: 'not_found', message: 'Sale not found' } };
-  }
-  return res.status(response.status).send(response.body);
+    if (!responseFromService) {
+      return res.status(404).json({ err: { code: 'not_found', message: 'Sale not found' } });
+    }
+    return res.status(response.status).send(response.body);
 };
 
-module.exports.updateSales = async (req, res) => {
-  const response = { ...constants.salesMessage };
+const updateSales = async (req, res) => {
+  const response = { ...constants.defaultServerResponse };
   try {
     const responseFromService = await salesService.updateSales(
       { id: req.params.id, updateInfo: req.body },
     );
     response.status = 200;
     response.body = responseFromService;
-    delete response.err;
+    return res.status(response.status).send(response.body);
   } catch (error) {
     response.status = 422;
-    response.body = { err: error.message };
+    response.err.code = constants.DATA_INVALID;
+    response.err.message = constants.ID_INVALID_OR_QUANTITY;
+    return res.status(response.status).send(response);
   }
-  return res.status(response.status).send(response.body);
 };
 
-module.exports.deleteSales = async (req, res) => {
-  const response = { ...constants.salesMessage };
-  try {
-    const responseFromService = await salesService.deleteSales(req.params);
-    response.status = 200;
-    response.body = responseFromService;
-  } catch (error) {
-    response.status = 422;
-    response.body = { err: { code: 'invalid_data', message: 'Wrong sale ID format' } };
-  }
-  return res.status(response.status).send(response.body);
+const deleteSales = async (req, res) => {
+  const { status, message } = await salesService.deleteSales(req.params);
+  return res.status(status).send(message);
+};
+
+module.exports = {
+  createSales,
+  getAllSales,
+  getSalesById,
+  updateSales,
+  deleteSales,
 };
