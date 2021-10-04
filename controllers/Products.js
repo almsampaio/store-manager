6b8f0591871f2595d0b7cdb88a57eff55d4308dc
Renan Costa <renancosta21@gmail.com>
2021-10-04 16:03:39 -0300

    feat: req2 getProductById controller

--- controllers/Products.js
+++ controllers/Products.js
@@ -1,4 +1,4 @@
-const rescue = require('express-rescue');
+// const rescue = require('express-rescue');
 const productService = require('../services/Products');
 
 const {
@@ -6,22 +6,34 @@ const {
   HTTP_CREATED_STATUS,
   HTTP_UNPROCESSED_STATUS } = require('../httpStatus/httpStatus');
 
-const getAll = rescue(async (req, res) => {
+const getAll = async (req, res) => {
   const products = await productService.getAll();
 
   res.status(HTTP_OK_STATUS).json(products);
-});
+};
 
-const create = rescue(async (req, res) => {
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  const product = await productService.getProductById(id);
+
+  if (product.err) {
+    return res.status(HTTP_UNPROCESSED_STATUS).json(product);
+  }
+
+  return res.status(HTTP_OK_STATUS).json(product);
+};
+
+const create = async (req, res) => {
   const { name, quantity } = req.body;
   const newProduct = await productService.create(name, quantity);
 
   if (newProduct.err) return res.status(HTTP_UNPROCESSED_STATUS).json(newProduct);
 
   return res.status(HTTP_CREATED_STATUS).json(newProduct);
-});
+};
 
 module.exports = {
   getAll,
   create,
+  getProductById,
 };
