20548a697d6141efc36a149179b2a0288c5774e2
Erick-Marinho <erickgaves@gmail.com>
2021-10-04 19:40:59 -0300

    Remoção de constantes

--- constants/index.js
+++ constants/index.js
@@ -7,26 +7,9 @@ module.exports = {
     },
     body: {},
   },
-  productMessage: {
-    PRODUCT_FETCHED: 'Product Fetched Successfuly',
-    PRODUCT_UPDATED: 'Product Updated Successfully',
-    PRODUCT_CREATED: 'Product Created Successfully',
-    PRODUCT_EXISTING: 'Product already exists',
-    PRODUCT_NOT_FOUND: 'Product Not Found',
-    DUPLICATE_NAME: 'Name already exist with given name',
-    DATA_INVALID: 'invalid_data',
-  },
-  salesMessage: {
-    status: '',
-    body: {},
-  },
-  sales: {
-    SALES_NOT_FOUND: 'Sales Not Found',
-  },
-  requestValidationMessage: {
-    BAD_REQUEST: 'Invalid fields',
-  },
-  dataBaseMessage: {
-    INVALID_ID: 'Wrong id format',
-  },
+  DATA_INVALID: 'invalid_data',
+  PRODUCT_EXISTS: 'Product already exists',
+  PRODUCT_NOT_FOUND: 'Product not found',
+  FORMAT_ID: 'Wrong id format',
+  ID_INVALID_OR_QUANTITY: 'Wrong product ID or invalid quantity',
 };
