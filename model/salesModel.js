1ded7c702bcfcd7eb716b74e77d4bd09952eec47
Erick-Marinho <erickgaves@gmail.com>
2021-10-04 19:41:58 -0300

    Inclusão de função

--- model/salesModel.js
+++ model/salesModel.js
@@ -1,9 +1,21 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
-module.exports.createSales = (productData) => (connection()
+const createSales = (productData) => (connection()
   .then((db) => db.collection('sales').insertOne({ itensSold: productData }))
   .then((result) => result.ops[0]))
   .catch((error) => {
     console.log('Unable to create sales ===', error);
     throw new Error(error);
   });
+
+const remove = async (id) => {
+  const db = await connection();
+  await db.collection('sales').deleteOne({ _id: ObjectId(id) });
+  return true;
+};
+
+module.exports = {
+  remove,
+  createSales,
+};
