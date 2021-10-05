3944f6ec2d0adf7f7fa991e6de9466031a3fff1d
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 19:26:56 -0400

    fix: req body disruption

--- controller/salesController.js
+++ controller/salesController.js
@@ -2,14 +2,24 @@ const service = require('../service/salesService');
 
 const addNewSale = async (req, res) => {
   try {
-    const { name, quantity } = req.body;
-    const operation = service.addNewSale({ name, quantity });
+    const { productId, quantity } = req.body;
+    const operation = service.addNewSale({ productId, quantity });
     res.status(200).json(operation);
   } catch (error) {
     res.status(400).json(error.message);
   }
 };
 
+const getAllSales = async (req, res) => {
+    try {
+      const sales = await service.getAllSales();
+      res.status(200).json(sales);
+    } catch (error) {
+      res.status(400).json(error.message);
+    }
+};
+
 module.exports = {
     addNewSale,
+    getAllSales,
 };
