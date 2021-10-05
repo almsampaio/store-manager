3ec82dda9eb46a7be41cb1aa4dec063eabe325bc
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 15:27:47 -0400

    feat: sales .js files

---
+++ model/salesModel.js
@@ -0,0 +1,39 @@
+const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const collection = 'sales';
+
+const addNewSale = async (newSale) => {
+    try {
+        const result = await connection().then((db) => db.collection(collection)
+          .insertOne(newSale));
+        return result.ops.pop();
+      } catch (error) {
+        return error.message;
+      }
+};
+
+const getAllSales = async () => {
+    try {
+      const sales = await connection().then((db) => db.collection(collection).find().toArray());
+      return sales;
+    } catch (error) {
+      return error.message;
+    }
+};
+
+const getSalesById = async (id) => {
+    try {
+      const sale = await connection().then((db) => db.collection(collection)
+      .find({ _id: ObjectId(id) }));
+      return sale;
+    } catch (error) {
+      return error.message;
+    }
+};
+
+module.exports = {
+    addNewSale,
+    getAllSales,
+    getSalesById,
+};
