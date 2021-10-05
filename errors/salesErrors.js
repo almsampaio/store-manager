321b0897aa0fe886db0b0efe582fe4ac70bf6a26
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 19:46:18 -0400

    fix: exporting now

--- errors/salesErrors.js
+++ errors/salesErrors.js
@@ -1,6 +1,10 @@
-const salesWrongData = {
+const invalidData = {
     err: {
       code: 'invalid_data',
       message: 'Wrong product ID or invalid quantity',
     },
+};
+
+module.exports = {
+    invalidData,
 };
