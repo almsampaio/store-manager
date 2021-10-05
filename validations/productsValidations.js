f4fd7f6731ed7612e6a9d41933d929601a9d33ef
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 19:59:10 -0400

    fix: conditions on validations

--- validations/productsValidations.js
+++ validations/productsValidations.js
@@ -2,21 +2,21 @@ const { ObjectId } = require('mongodb');
 const service = require('../service/productsService');
 
 const validateInsertedName = async (req, res, next) => {
-    const { name } = req.body;
-    const allProducts = await service.getAllProducts();
-    if (name.length < 5) {
-      res.status(422).json({ err: { 
-          code: 'invalid_data',
-          message: '\'name\' length must be at least 5 characters long',
-       } });
-    }
-    if (allProducts.some((product) => product.name === name)) {
-      res.status(422).json({ err: { 
-          code: 'invalid_data',
-          message: 'Product already exists',
-      } });
-    }
+  const { name } = req.body;
+  const allProducts = await service.getAllProducts();
+  if (name.length < 5) {
+    res.status(422).json({ err: { 
+        code: 'invalid_data',
+        message: '\'name\' length must be at least 5 characters long',
+     } });
+  } else if (allProducts.some((product) => product.name === name)) {
+    res.status(422).json({ err: { 
+        code: 'invalid_data',
+        message: 'Product already exists',
+    } });
+  } else {
     next();
+  }
 };
 
 const validateInsertedQtd = (req, res, next) => {
@@ -26,14 +26,14 @@ const validateInsertedQtd = (req, res, next) => {
         code: 'invalid_data',
         message: '\'quantity\' must be larger than or equal to 1',
     } });
-  }
-  if (typeof quantity !== 'number') {
+  } else if (typeof quantity !== 'number') {
     res.status(422).json({ err: { 
         code: 'invalid_data',
         message: '\'quantity\' must be a number',
     } });
-  }
+  } else {
   next();
+  }
 };
 
 const validateObjectID = async (req, res, next) => {
@@ -43,8 +43,9 @@ const validateObjectID = async (req, res, next) => {
         code: 'invalid_data',
         message: 'Wrong id format',
     } });
+  } else {
+    next();
   }
-  next();
 };
 
 module.exports = {
