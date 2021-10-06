77a19c13daaef71a07dcaf04b5fd96afade9aa44
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 20:59:50 -0400

    refactor: changing from insertingOne w/forEach to insertMany

--- controller/salesController.js
+++ controller/salesController.js
@@ -2,17 +2,19 @@ const service = require('../service/salesService');
 
 const addNewSale = async (req, res) => {
   try {
-    req.body.forEach((sale) => {
-    const { productId, quantity } = sale;
-    const operation = service.addNewSale({ productId, quantity });
-    res.status(200).json(operation);
+    const newSales = [];
+    await req.body.forEach(({ productId, quantity }) => {
+      const sale = { productId, quantity };
+      newSales.push(sale);
     });
+    const operation = await service.addNewSale(newSales);
+    res.status(200).json(operation);
   } catch (error) {
     res.status(400).json(error.message);
   }
 };
 
-const getAllSales = async (req, res) => {
+const getAllSales = async (_req, res) => {
     try {
       const sales = await service.getAllSales();
       res.status(200).json(sales);
