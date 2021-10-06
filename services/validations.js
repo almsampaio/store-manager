ea50dd37113f452d806fca960415be231a23aee7
jlfagundes <jlfagundes@gmail.com>
2021-10-06 11:26:09 -0300

    fix: validations line 53

--- services/validations.js
+++ services/validations.js
@@ -2,11 +2,8 @@ const modelProduct = require('../models/products');
 
 const validName = (name) => {
   if (name.length < 5) {
-    return {
-      err: {
-        code: 'invalid_data',
-        message: '"name" length must be at least 5 characters long',
-      },
+    return { 
+      err: { code: 'invalid_data', message: '"name" length must be at least 5 characters long' },
     };
   }
 };
@@ -49,37 +46,39 @@ const notNumberQuantity = (quantity) => {
   return false;
 };
 
-const verifyStockNewSale = async (salesArray) => {
-  const { itensSold } = salesArray
-  const verifyStockAllProducts = itensSold.map(async (product) => {
-    const { productId, quantity } = product;
-    if (quantity) {
-      const findProduct = await modelProduct.searchById(productId);
-      const newStock = findProduct.quantity - quantity;
-      if (newStock < 0) {
-        return {
-          err: { code: 'stock_problem', message: 'Such amount is not permitted to sell ' },
-        }; 
-       }
-    return true;
-    }
-    return false;
-  });
-  return verifyStockAllProducts;
-};
+// const verifyStockNewSale = async (salesArray) => {
+//   const { itensSold } = salesArray
+//   const verifyStockAllProducts = itensSold.map(async (product) => {
+//     const { productId, quantity } = product;
+//     if (quantity) {
+//       const findProduct = await modelProduct.searchById(productId);
+//       const newStock = findProduct.quantity - quantity;
+//       if (newStock < 0) {
+//         return {
+//           err: { code: 'stock_problem', message: 'Such amount is not permitted to sell ' },
+//         }; 
+//        }
+//     return true;
+//     }
+//     return false;
+//   });
+//   return verifyStockAllProducts;
+// };
 
 const validSales = async (salesArray) => {
   const verifyQuantity = await salesArray.find((sale) => sale.quantity <= 0);
 
   const verifyTypeQuantity = await salesArray.find((sale) => typeof sale.quantity !== 'number');
 
+  const erro = {
+    err: {
+      code: 'invalid_data',
+      message: 'Wrong product ID or invalid quantity',
+    },
+  };
+
   if (verifyQuantity || verifyTypeQuantity) {
-    return {
-      err: {
-        code: 'invalid_data',
-        message: 'Wrong product ID or invalid quantity',
-      },
-    };
+    return erro;
   }
 };
 
@@ -89,5 +88,5 @@ module.exports = {
   notQuantityNegative,
   notNumberQuantity,
   validSales,
-  verifyStockNewSale,
+  // verifyStockNewSale,
 };
