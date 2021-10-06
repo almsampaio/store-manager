8c36bb4929fc43cc882831df94fbe5934f1bc60d
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 21:09:49 -0400

    refactor: returning res

--- validations/productsValidations.js
+++ validations/productsValidations.js
@@ -5,47 +5,46 @@ const validateInsertedName = async (req, res, next) => {
   const { name } = req.body;
   const allProducts = await service.getAllProducts();
   if (name.length < 5) {
-    res.status(422).json({ err: { 
+    return res.status(422).json({ err: { 
         code: 'invalid_data',
         message: '\'name\' length must be at least 5 characters long',
      } });
-  } else if (allProducts.some((product) => product.name === name)) {
-    res.status(422).json({ err: { 
+  } 
+  if (allProducts.some((product) => product.name === name)) {
+    return res.status(422).json({ err: { 
         code: 'invalid_data',
         message: 'Product already exists',
     } });
-  } else {
-    next();
   }
+  return next();
 };
 
 const validateInsertedQtd = (req, res, next) => {
   const { quantity } = req.body;
   if (quantity <= 0) {
-    res.status(422).json({ err: { 
+    return res.status(422).json({ err: { 
         code: 'invalid_data',
         message: '\'quantity\' must be larger than or equal to 1',
     } });
-  } else if (typeof quantity !== 'number') {
-    res.status(422).json({ err: { 
+  }
+  if (typeof quantity !== 'number') {
+    return res.status(422).json({ err: { 
         code: 'invalid_data',
         message: '\'quantity\' must be a number',
     } });
-  } else {
-  next();
-  }
+  } 
+  return next();
 };
 
 const validateObjectID = async (req, res, next) => {
   const { id } = req.params;
   if (!ObjectId.isValid(id)) {
-    res.status(422).json({ err: { 
+    return res.status(422).json({ err: { 
         code: 'invalid_data',
         message: 'Wrong id format',
     } });
-  } else {
-    next();
-  }
+  } 
+  return next();
 };
 
 module.exports = {
