3c9ef26c58a6847da1a934fd16a4c00305eb8cbb
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-05 21:01:04 -0400

    refactor: returning res and next

--- validations/salesValidations.js
+++ validations/salesValidations.js
@@ -2,18 +2,17 @@ const { ObjectId } = require('mongodb');
 const errors = require('../errors/salesErrors');
 
 const validateInsertedData = async (req, res, next) => {
-  req.body.forEach((sale) => {
-    const { productId, quantity } = sale;
-    if (typeof quantity !== 'number') {
-      res.status(422).json(errors.invalidData);
-    } else if (quantity <= 0) {
-      res.status(422).json(errors.invalidData);
-    } else if (!ObjectId.isValid(productId)) {
-      res.status(422).json(errors.invalidData);
-    } else {
-      next();
-    }
-  });
+  await req.body.forEach(async (sale) => {
+      const { productId, quantity } = sale;
+      if (typeof quantity !== 'number') {
+        return res.status(422).json(errors.invalidData);
+      } if (quantity <= 0) {
+        return res.status(422).json(errors.invalidData);
+      } if (!ObjectId.isValid(productId)) {
+        return res.status(422).json(errors.invalidData);
+      }
+    });
+  next();
 };
 
 module.exports = {
