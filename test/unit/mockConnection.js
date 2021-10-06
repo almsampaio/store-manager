ff7a537aef4c32183c0345939b099fd27acaf3c2
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-10-06 11:23:21 -0300

    alterando mockConnection

--- test/unit/mockConnection.js
+++ test/unit/mockConnection.js
@@ -1,15 +1,14 @@
 const { MongoClient } = require('mongodb');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
-const DB_NAME = 'StoreManager';
+
 const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true }
 
 const getConnection = async () => {
   const DBServer = await MongoMemoryServer.create();
   const URLMock = DBServer.getUri();
 
-  return MongoClient.connect(URLMock, OPTIONS)
-    .then((connection) => connection.db(DB_NAME));
+  return MongoClient.connect(URLMock, OPTIONS);
 };
 
 module.exports = getConnection;
