8270a47149d919dca0aeccee312bd6a28755fdda
Eduardo Pizorno <pizorno@gmail.com>
2021-10-06 12:00:59 -0300

    iniciando o projeto

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
+// add commit inicial
+app.get("/", (_request, response) => {
   response.send();
 });
