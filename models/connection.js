b6e3942839afe7717179efa06d40fa1839ab2e2b
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-06 13:04:30 -0400

    fix: db url

--- models/connection.js
+++ models/connection.js
@@ -5,7 +5,7 @@ const OUTPUT = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = () => MongoClient
