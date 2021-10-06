942edeac58419769eae997206c19ba48026b966b
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-10-06 11:43:33 -0400

    feat: test mock conn and first unit test

---
+++ test/unit/connection.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const OUTPUT = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  const connect = async () => {
+      const DBServer = new MongoMemoryServer();
+    const URLMOCK = await DBServer.getUri();
+    return MongoClient.connect(URLMOCK, OUTPUT).then((conn) => conn.db('StoreManager'))
+};
+
+module.exports = connect;
