6e05eedc49b3cd59ea0ddf0d13aa9e6564a42cf0
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-10-05 21:38:12 -0300

    tentando mais uma vez

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 const OPTIONS = { useNewUrlParser: true, newUnifiedTopology: true };
 
