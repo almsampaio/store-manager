8122e3a36c0c2bbfacdad564f97104fb11cf9715
Mateus Santiago <mateussantiago.victor@gmail.com>
2021-10-06 10:17:30 -0300

    atualizando PR

--- models/connection.js
+++ models/connection.js
@@ -4,11 +4,13 @@ const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 const OPTIONS = { useNewUrlParser: true, newUnifiedTopology: true };
 
-module.exports = () => {
-  MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((connection) => connection.db(DB_NAME))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+const connection = () => MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => conn.db(DB_NAME))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+module.exports = {
+  connection,
 };
